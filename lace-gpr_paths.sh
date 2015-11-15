bb82a3d53d0f45ed0e800630025c9a6f7469cfb4
Rod Kay <rodakay@internode.on.net>
2015-11-16 04:07:43 +1100

    Added MMI paths to lace-gpr_paths.sh.

--- lace-gpr_paths.sh
+++ lace-gpr_paths.sh
@@ -7,16 +7,18 @@ ADA_PROJECT_PATH=$LACE/1-base/xml/library:$ADA_PROJECT_PATH
 ADA_PROJECT_PATH=$LACE/2-low/collada/library:$ADA_PROJECT_PATH
 
 
+
 ADA_PROJECT_PATH=$LACE/3-mid/opengl/library:$ADA_PROJECT_PATH
 
-
-#ADA_PROJECT_PATH=$LACE/3-mid/physics/bullet/contrib/bullet-2.79/library:$ADA_PROJECT_PATH
 ADA_PROJECT_PATH=$LACE/3-mid/physics/bullet/library:$ADA_PROJECT_PATH
-
-#ADA_PROJECT_PATH=$LACE/3-mid/physics/box2d/contrib/Box2D/library:$ADA_PROJECT_PATH
 ADA_PROJECT_PATH=$LACE/3-mid/physics/box2d/library:$ADA_PROJECT_PATH
-
 ADA_PROJECT_PATH=$LACE/3-mid/physics/interface/library:$ADA_PROJECT_PATH
+
+
+
+ADA_PROJECT_PATH=$LACE/4-high/mmi/library:$ADA_PROJECT_PATH
+ADA_PROJECT_PATH=$LACE/4-high/mmi/library/lumen:$ADA_PROJECT_PATH
+ADA_PROJECT_PATH=$LACE/4-high/mmi/library/sdl:$ADA_PROJECT_PATH
 
 
 export ADA_PROJECT_PATH
