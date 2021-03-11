1a493f0826cd15a4b0ddfdd89eb9fc0ad3edae82
Rod Kay <rodakay5@gmail.com>
2021-03-12 03:31:26 +1100

    lace: Use GPR_PROJECT_PATH_PATH rather than ADA_PROJECT_PATH.

--- lace-gpr_paths.sh
+++ lace-gpr_paths.sh
@@ -1,24 +1,24 @@
-ADA_PROJECT_PATH=$LACE/1-base/lace/library:$ADA_PROJECT_PATH
-ADA_PROJECT_PATH=$LACE/1-base/math/library:$ADA_PROJECT_PATH
-ADA_PROJECT_PATH=$LACE/1-base/xml/library:$ADA_PROJECT_PATH
+GPR_PROJECT_PATH=$LACE/1-base/lace/library:$GPR_PROJECT_PATH
+GPR_PROJECT_PATH=$LACE/1-base/math/library:$GPR_PROJECT_PATH
+GPR_PROJECT_PATH=$LACE/1-base/xml/library:$GPR_PROJECT_PATH
 
 
-ADA_PROJECT_PATH=$LACE/2-low/collada/library:$ADA_PROJECT_PATH
-ADA_PROJECT_PATH=$LACE/2-low/neural/library:$ADA_PROJECT_PATH
+GPR_PROJECT_PATH=$LACE/2-low/collada/library:$GPR_PROJECT_PATH
+GPR_PROJECT_PATH=$LACE/2-low/neural/library:$GPR_PROJECT_PATH
 
 
-ADA_PROJECT_PATH=$LACE/3-mid/opengl/library:$ADA_PROJECT_PATH
+GPR_PROJECT_PATH=$LACE/3-mid/opengl/library:$GPR_PROJECT_PATH
 
-ADA_PROJECT_PATH=$LACE/3-mid/physics/interface/library:$ADA_PROJECT_PATH
-ADA_PROJECT_PATH=$LACE/3-mid/physics/implement/c_math/library:$ADA_PROJECT_PATH
-ADA_PROJECT_PATH=$LACE/3-mid/physics/implement/bullet/library:$ADA_PROJECT_PATH
-ADA_PROJECT_PATH=$LACE/3-mid/physics/implement/box2d/library:$ADA_PROJECT_PATH
-ADA_PROJECT_PATH=$LACE/3-mid/physics/implement/impact/library:$ADA_PROJECT_PATH
-
-
-ADA_PROJECT_PATH=$LACE/4-high/mmi/library:$ADA_PROJECT_PATH
-ADA_PROJECT_PATH=$LACE/4-high/mmi/library/lumen:$ADA_PROJECT_PATH
-ADA_PROJECT_PATH=$LACE/4-high/mmi/library/sdl:$ADA_PROJECT_PATH
+GPR_PROJECT_PATH=$LACE/3-mid/physics/interface/library:$GPR_PROJECT_PATH
+GPR_PROJECT_PATH=$LACE/3-mid/physics/implement/c_math/library:$GPR_PROJECT_PATH
+GPR_PROJECT_PATH=$LACE/3-mid/physics/implement/bullet/library:$GPR_PROJECT_PATH
+GPR_PROJECT_PATH=$LACE/3-mid/physics/implement/box2d/library:$GPR_PROJECT_PATH
+GPR_PROJECT_PATH=$LACE/3-mid/physics/implement/impact/library:$GPR_PROJECT_PATH
 
 
-export ADA_PROJECT_PATH
+GPR_PROJECT_PATH=$LACE/4-high/mmi/library:$GPR_PROJECT_PATH
+GPR_PROJECT_PATH=$LACE/4-high/mmi/library/lumen:$GPR_PROJECT_PATH
+GPR_PROJECT_PATH=$LACE/4-high/mmi/library/sdl:$GPR_PROJECT_PATH
+
+
+export GPR_PROJECT_PATH
