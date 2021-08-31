4d35695296be5b74494d8e1dd76d501ab0922ca0
Rod Kay <rodakay5@gmail.com>
2021-08-31 15:37:14 +1000

    opengl: Tidy 'launch_many_boxes_demo'.

--- 3-mid/opengl/applet/demo/culler/many_boxes/launch_many_boxes_demo.adb
+++ 3-mid/opengl/applet/demo/culler/many_boxes/launch_many_boxes_demo.adb
@@ -1,16 +1,11 @@
 with
      openGL.Palette,
-     openGL.Texture,
-     openGL.Model.box.lit_colored_textured,
+     openGL.Model.Box.lit_colored_textured,
      openGL.Visual,
 
-     openGL.Demo,
+     openGL.Demo;
 
-     ada.Text_IO,
-     ada.Exceptions;
-
-
-procedure launch_many_boxes_Demo
+procedure launch_many_Boxes_Demo
 --
 --  Exercise the culler with many boxes.
 --
@@ -20,8 +15,7 @@ is
        openGL.Model.box,
        openGL.Palette,
        openGL.Math,
-       openGL.linear_Algebra_3d,
-       ada.Text_IO;
+       openGL.linear_Algebra_3d;
 
 begin
    openGL.Demo.define ("openGL 'many Boxes' Demo");
@@ -32,53 +26,36 @@ begin
                             y_Rotation_from (to_Radians (0.0)));
 
    declare
-      use openGL.Math.Functions;
+      Face          : constant asset_Name := to_Asset ("assets/Face1.bmp");
 
-      the_box_Model : constant openGL.Model.box.lit_colored_textured.view
-        := openGL.Model.box.lit_colored_textured.new_Box
+      the_box_Model : constant Box.lit_colored_textured.view
+        := Box.lit_colored_textured.new_Box
           (size          => (0.5, 0.5, 0.5),
-           faces         => (front => (colors         => (others => (White,     Opaque)),
-                                       texture_name   => to_Asset ("assets/Face1.bmp")),
-                                       --  texture_object => Texture.null_Object),
-                             rear  => (colors         => (others => (Blue,     Opaque)),
-                                       texture_name   => to_Asset ("assets/Face1.bmp")),
-                                       --  texture_object => Texture.null_Object),
-                             upper => (colors         => (others => (Green,    Opaque)),
-                                       texture_name   => to_Asset ("assets/Face1.bmp")),
-                                       --  texture_object => Texture.null_Object),
-                             lower => (colors         => (others => (Green,    Opaque)),
-                                       texture_name   => to_Asset ("assets/Face1.bmp")),
-                                       --  texture_object => Texture.null_Object),
-                             left  => (colors         => (others => (Dark_Red, Opaque)),
-                                       texture_name   => to_Asset ("assets/Face1.bmp")),
-                                       --  texture_object => Texture.null_Object),
-                             right => (colors         => (others => (Red,      Opaque)),
-                                       texture_name   => to_Asset ("assets/Face1.bmp"))));
-                                       --  texture_object => Texture.null_Object)));
+           faces         => (front => (colors => (others => (White,    Opaque)), texture_name => Face),
+                             rear  => (colors => (others => (Blue,     Opaque)), texture_name => Face),
+                             upper => (colors => (others => (Green,    Opaque)), texture_name => Face),
+                             lower => (colors => (others => (Green,    Opaque)), texture_name => Face),
+                             left  => (colors => (others => (Dark_Red, Opaque)), texture_name => Face),
+                             right => (colors => (others => (Red,      Opaque)), texture_name => Face)));
 
-      --  The Sprites.
-      --
---        the_Sprites : openGL.Sprite.views (1 .. 1) := (others => new_Sprite (the_box_Model_2));
---        the_Sprites : openGL.Sprite.views (1 .. 5_000) := (others => new_Sprite (the_box_Model_2));
-      the_Sprites : constant openGL.Visual.views (1 .. 5_000) := (others => openGL.Visual.Forge.new_Visual (openGL.Model.view (the_box_Model)));
---        the_Sprites : openGL.Sprite.views (1 .. 10_000) := (others => new_Sprite (the_box_Model_2));
+      Size : constant Integer     :=  70;
+      x    :          openGL.Real := -openGL.Real (Size) / 2.0;
+      z    :          openGL.Real :=  0.0;
 
-      grid_Size   : constant openGL.Real :=  SqRt (openGL.Real (the_Sprites'Length));
-      x           :          openGL.Real := -grid_Size / 2.0;
-      z           :          openGL.Real :=  0.0;
+      Sprites : constant Visual.views (1 .. Size * Size) := (others => Visual.Forge.new_Visual (Model.view (the_box_Model)));
 
    begin
-      for i in the_Sprites'Range
+      for i in Sprites'Range
       loop
          x := x + 1.0;
 
-         if i mod Integer (SqRt (openGL.Real (the_Sprites'Length))) = 0
+         if i mod Size = 0
          then
             z := z - 1.0;
-            x := -grid_Size / 2.0;
+            x := -openGL.Real (Size) / 2.0;
          end if;
 
-         the_Sprites (i).Site_is ((x, 0.0, z));
+         Sprites (i).Site_is ((x, 0.0, z));
       end loop;
 
 
@@ -89,22 +66,7 @@ begin
          Demo.Dolly.evolve;
          Demo.Done := Demo.Dolly.quit_Requested;
 
-         declare
-            Command : Character;
-            Avail   : Boolean;
-         begin
-            Demo.Dolly.get_last_Character (Command, Avail);
-
-            if Avail
-            then
-               case Command
-               is
-               when others => null;
-               end case;
-            end if;
-         end;
-
-         Demo.Camera.render (the_Sprites);
+         Demo.Camera.render (Sprites);
 
          while not Demo.Camera.cull_Completed
          loop
@@ -117,15 +79,4 @@ begin
    end;
 
    Demo.destroy;
-   new_Line;
-
-
-   new_Line;
-
-exception
-   when E : others =>
-      new_Line;
-      put_Line ("Unhandled exception in main thread !");
-      put_Line (Ada.Exceptions.Exception_Information (E));
-      new_Line;
-end launch_many_boxes_Demo;
+end launch_many_Boxes_Demo;
