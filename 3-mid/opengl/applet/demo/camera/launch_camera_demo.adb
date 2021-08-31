8509d19b2dc781db538c7137d063de715acd1e79
Rod Kay <rodakay5@gmail.com>
2021-08-31 14:13:54 +1000

    opengl: Tidy 'launch_camera_demo'.

--- 3-mid/opengl/applet/demo/camera/launch_camera_demo.adb
+++ 3-mid/opengl/applet/demo/camera/launch_camera_demo.adb
@@ -3,16 +3,10 @@ with
      openGL.Camera,
      openGL.Visual,
      openGL.Palette,
-     openGL.Server,
      openGL.Model.box.colored,
-     openGL.Demo,
+     openGL.Demo;
 
-     lumen.Window,
-
-     ada.Text_IO;
-
-
-procedure launch_camera_Demo
+procedure launch_Camera_Demo
 --
 --  Exercise the camera.
 --
@@ -22,48 +16,30 @@ is
        openGL.Model.box,
        openGL.Palette,
        openGL.Math,
-       openGL.linear_Algebra_3d,
-       ada.Text_IO;
-
---     Win          :          Lumen .Window.Window_Handle;
---     the_Camera   :          openGL.Camera.item;
---     the_Renderer : aliased  openGL.Renderer.lean.item;
+       openGL.linear_Algebra_3d;
 
 begin
    openGL.Demo.define ("openGL 'Camera' Demo");
 
---     --  Create Lumen window (which provides a current GL context).
---     --
---     Lumen.Window.Create (Win,
---                          width    => 1000,
---                          height   => 1000,
---                          name     => "openGL 'Camera' Demo",
---                          animated => True);
---
---     put_Line ("openGL Server: " & openGL.Server.Version);
-
-
    --  Setup the camera.
    --
---     the_Camera.define;
---     the_Camera.Renderer_is (the_Renderer'unchecked_Access);
    Demo.Camera.Position_is ((5.0, 0.0, 10.0),
                             y_Rotation_from (to_Radians (0.0)));
 
    declare
-      --  The Models.
+      --  The Model.
       --
-      the_box_Model_1 : constant openGL.Model.box.colored.view
-        := openGL.Model.box.colored.new_Box (size  => (0.5, 0.5, 0.5),
-                                             faces => (front => (colors => (others => (Blue,            Opaque))),
-                                                       rear  => (colors => (others => (light_Blue,      Opaque))),
-                                                       upper => (colors => (others => (Green,           Opaque))),
-                                                       lower => (colors => (others => (forest_Green,    Opaque))),
-                                                       left  => (colors => (others => (Dark_Red,        Opaque))),
-                                                       right => (colors => (others => (Red,             Opaque)))));
+      the_box_Model : constant openGL.Model.Box.colored.view
+        := openGL.Model.Box.colored.new_Box (size  => (0.5, 0.5, 0.5),
+                                             faces => (front => (colors => (others => (Blue,         Opaque))),
+                                                       rear  => (colors => (others => (light_Blue,   Opaque))),
+                                                       upper => (colors => (others => (Green,        Opaque))),
+                                                       lower => (colors => (others => (forest_Green, Opaque))),
+                                                       left  => (colors => (others => (Dark_Red,     Opaque))),
+                                                       right => (colors => (others => (Red,          Opaque)))));
 
-      the_Sprite     : constant openGL.Visual.view
-        := openGL.Visual.Forge.new_Visual (the_box_Model_1.all'Access);
+      the_Sprite : constant openGL.Visual.view
+        := openGL.Visual.Forge.new_Visual (the_box_Model.all'Access);
 
    begin
       the_Sprite.Site_is ((10.0, 0.0, 0.0));
@@ -89,5 +65,4 @@ begin
    end;
 
    Demo.destroy;
-   new_Line;
-end launch_camera_Demo;
+end launch_Camera_Demo;
