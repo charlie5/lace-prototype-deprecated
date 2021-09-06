050db51cf3c573844609db0c51770b7e490f1270
Rod Kay <rodakay5@gmail.com>
2021-09-06 21:26:20 +1000

    opengl.raster: Cosmetics.

--- 3-mid/opengl/source/desk/opengl-raster.adb
+++ 3-mid/opengl/source/desk/opengl-raster.adb
@@ -1,13 +1,13 @@
 package body openGL.Raster
 is
 
-   procedure set_window_Pos (x, y : in Real;
-                             z    : in Real := 0.0;
-                             w    : in Real := 1.0)
+   procedure set_Window_Position (X, Y : in Real;
+                                  Z    : in Real := 0.0;
+                                  W    : in Real := 1.0)
    is
-      pragma Unreferenced (z, w);
+      pragma Unreferenced (Z, W);
    begin
-      raise Program_Error with "unimplemented";
-   end set_window_Pos;
+      raise Program_Error with "TODO: unimplemented";
+   end set_Window_Position;
 
 end openGL.Raster;
--- 3-mid/opengl/source/desk/opengl-raster.ads
+++ 3-mid/opengl/source/desk/opengl-raster.ads
@@ -4,8 +4,8 @@ package openGL.Raster
 --
 is
 
-   procedure set_window_Pos (x, y : in Real;
-                             z    : in Real := 0.0;    -- Default of '0.0' places text in front of all other renders.
-                             w    : in Real := 1.0);
+   procedure set_Window_Position (X, Y : in Real;
+                                  Z    : in Real := 0.0;    -- Default of '0.0' places text in front of all other renders.
+                                  W    : in Real := 1.0);
 
 end openGL.Raster;
