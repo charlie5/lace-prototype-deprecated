54d3a898dfc2ce38fa9b3868b15f39362e31ee35
Rod Kay <rodakay5@gmail.com>
2021-09-11 03:18:12 +1000

    opengl.model.sphere.colored: Cosmetics.

--- 3-mid/opengl/source/lean/model/opengl-model-sphere-colored.adb
+++ 3-mid/opengl/source/lean/model/opengl-model-sphere-colored.adb
@@ -1,7 +1,6 @@
 with
-     openGL.Geometry.colored,
+     openGL.Geometry .colored,
      openGL.Primitive.indexed;
-
 
 package body openGL.Model.sphere.colored
 is
@@ -9,76 +8,72 @@ is
    --- Forge
    --
 
-   function new_Sphere (Radius : in math.Real;
-                        Color  : in openGL.lucid_Color) return View
+   function new_Sphere (Radius : in Real;
+                        Color  : in lucid_Color) return View
    is
       Self : constant View := new Item;
    begin
       Self.Color := Color;
-      Self.define (scale => (Radius * 2.0,
+      Self.define (Scale => (Radius * 2.0,
                              Radius * 2.0,
                              Radius * 2.0));
       return Self;
    end new_Sphere;
 
 
-
    --------------
    --- Attributes
    --
 
-   type Geometry_view is access all openGL.Geometry.colored.item'Class;
+   type Geometry_view is access all Geometry.colored.item'Class;
 
 
-
-   --  nb: - an extra vertex is required at the end of each latitude ring
-   --     - this last vertex has the same site as the rings initial vertex.
-   --     - the  last    vertex has 's' texture coord of 1.0, whereas
-   --       the  initial vertex has 's' texture coord of 0.0
+   -- NB: - An extra vertex is required at the end of each latitude ring.
+   --     - This last vertex has the same site as the rings initial vertex.
+   --     - The  last    vertex has 's' texture coord of 1.0, whereas
+   --       the  initial vertex has 's' texture coord of 0.0.
    --
    overriding
-   function  to_GL_Geometries (Self : access Item;   Textures : access Texture.name_Map_of_texture'Class;
-                                                     Fonts    : in     Font.font_id_Maps_of_font.Map) return openGL.Geometry.views
+   function to_GL_Geometries (Self : access Item;   Textures : access Texture.name_Map_of_texture'Class;
+                                                    Fonts    : in     Font.font_id_Map_of_font) return Geometry.views
    is
-      pragma Unreferenced (Textures, Fonts);
+      pragma unreferenced (Textures, Fonts);
 
-      use openGL.Geometry,
-          openGL.Geometry.colored;
+      use Geometry,
+          Geometry.colored;
 
-      Degrees_360       : constant      := Pi * 2.0;
-      Degrees_180       : constant      := Pi;
+      Degrees_360    : constant := Pi * 2.0;
+      Degrees_180    : constant := Pi;
 
-      latitude_Count    : constant      :=  2 * 13;
-      longitude_Count   : constant      :=  2 * 26;
+      lat_Count      : constant :=  2 * 13;
+      long_Count     : constant :=  2 * 26;
 
-      Num_lat_strips    : constant      := latitude_Count - 1;
+      Num_lat_strips : constant := lat_Count - 1;
 
-      latitude_Spacing  : constant Real := Degrees_180 / Real (latitude_Count - 1);
-      longitude_Spacing : constant Real := Degrees_360 / Real (longitude_Count);
+      lat_Spacing    : constant Real := Degrees_180 / Real (lat_Count - 1);
+      long_Spacing   : constant Real := Degrees_360 / Real (long_Count);
 
-      vertex_Count      : constant openGL.Index_t      :=   1 + 1                                           -- North and south pole.
-                                                          + (longitude_Count + 1) * (latitude_Count - 2);   -- Each latitude ring.
+      indices_Count  : constant long_Index_t := Num_lat_strips * (long_Count + 1) * 2;
+      vertex_Count   : constant      Index_t :=   1 + 1                                 -- North and south pole.
+                                                + (long_Count + 1) * (lat_Count - 2);   -- Each latitude ring.
 
-      indices_Count     : constant openGL.long_Index_t := Num_lat_strips * (longitude_Count + 1) * 2;
+      the_Sites      : aliased  Sites                         := (1 ..  vertex_Count => <>);
+      the_Indices    : aliased  Indices                       := (1 .. indices_Count => <>);
+      the_Vertices   : aliased  Geometry.colored.Vertex_array := (1 ..  vertex_Count => <>);
 
-      the_Sites         : aliased  openGL.Sites        := (1 .. vertex_Count => <>);
-
-      the_Vertices      : aliased  openGL.geometry.colored.Vertex_array := (1 .. vertex_Count  => <>);
-      the_Indices       : aliased  Indices                              := (1 .. indices_Count => <>);
-
-      the_Geometry      : constant Geometry_view := Geometry_view (openGL.Geometry.colored.new_Geometry);
+      the_Geometry   : constant Geometry_view := Geometry.colored.new_Geometry;
 
    begin
-      set_Sites :
+      set_Sites:
       declare
          use linear_Algebra_3d;
 
-         north_Pole : constant Site    := (0.0,  0.5,  0.0);
-         south_Pole : constant Site    := (0.0, -0.5,  0.0);
+         north_Pole : constant Site := (0.0,  0.5,  0.0);
+         south_Pole : constant Site := (0.0, -0.5,  0.0);
 
-         the_Site   :          Site    := north_Pole;
-         vert_Id    :          Index_t := 1;              -- Start at '1' (not '0')to account for north pole.
-         a, b       :          Real    := 0.0;            -- Angular 'cursors' used to track lat/long for texture coords.
+         the_Site : Site    := north_Pole;
+         vert_Id  : Index_t := 1;              -- Start at '1' (not '0')to account for north pole.
+         a, b     : Real    := 0.0;            -- Angular 'cursors' used to track lat/long for texture coords.
 
          latitude_line_First : Site;
 
@@ -91,26 +86,26 @@ is
          the_Vertices (the_Vertices'Last).Site   := south_Pole;
          the_Vertices (the_Vertices'Last).Color  := Self.Color;
 
-         for lat_Id in 2 .. latitude_Count - 1
+         for lat_Id in 2 .. lat_Count - 1
          loop
             a := 0.0;
-            b := b + latitude_Spacing;
+            b := b + lat_Spacing;
 
-            the_Site            := the_Site * z_rotation_from (latitude_Spacing);
+            the_Site            := the_Site * z_Rotation_from (lat_Spacing);
             latitude_line_First := the_Site;                      -- Store initial latitude lines 1st point.
 
             vert_Id             := vert_Id + 1;
             the_Sites (vert_Id) := the_Site;                      -- Add 1st point on a line of latitude.
 
-            the_Vertices (vert_Id).Site   := the_Site;
-            the_Vertices (vert_Id).Color  := Self.Color;
+            the_Vertices (vert_Id).Site  := the_Site;
+            the_Vertices (vert_Id).Color := Self.Color;
 
-            for long_Id in 1 .. longitude_Count
+            for long_Id in 1 .. long_Count
             loop
-               a := a + longitude_Spacing;
+               a := a + long_Spacing;
 
-               if long_Id /= longitude_Count
-               then   the_Site := the_Site * y_rotation_from (-longitude_Spacing);
+               if long_Id /= long_Count
+               then   the_Site := the_Site * y_Rotation_from (-long_Spacing);
                else   the_Site := latitude_line_First;             -- Restore the_Vertex back to initial latitude lines 1st point.
                end if;
 
@@ -125,56 +120,48 @@ is
       end set_Sites;
 
 
-      set_Indices :
+      set_Indices:
       declare
          strip_Id : long_Index_t := 0;
 
-         Upper    : Index_t;
-         Lower    : Index_t;
-
-         procedure set
-         is
-         begin
-            strip_Id := strip_Id + 1;   the_Indices (strip_Id) := Upper;
-            strip_Id := strip_Id + 1;   the_Indices (strip_Id) := Lower;
-         end set;
-
+         Upper : Index_t;
+         Lower : Index_t;
       begin
          upper := 1;
          lower := 2;
 
          for lat_Strip in 1 .. num_lat_Strips
          loop
-            for Each in 1 .. longitude_Count + 1
+            for Each in 1 .. long_Count + 1
             loop
-               set;
+               strip_Id := strip_Id + 1;   the_Indices (strip_Id) := Upper;
+               strip_Id := strip_Id + 1;   the_Indices (strip_Id) := Lower;
 
                if lat_Strip /= 1              then   Upper := Upper + 1;   end if;
                if lat_Strip /= num_lat_Strips then   Lower := Lower + 1;   end if;
             end loop;
 
-
             if lat_Strip = 1 then
                Upper := 2;
             end if;
 
-            Lower := Upper + longitude_Count + 1;
+            Lower := Upper + long_Count + 1;
          end loop;
       end set_Indices;
 
 
       the_Geometry.is_Transparent (False);
-      Vertices_are (the_Geometry.all,  the_Vertices);
+      the_Geometry.Vertices_are   (the_Vertices);
 
       declare
-         the_Primitive : constant openGL.Primitive.indexed.view
-           := openGL.Primitive.indexed.new_Primitive (primitive.triangle_Strip,  the_Indices);
+         the_Primitive : constant Primitive.indexed.view
+           := Primitive.indexed.new_Primitive (Primitive.triangle_Strip,
+                                               the_Indices);
       begin
-         the_Geometry.add (openGL.Primitive.view (the_Primitive));
+         the_Geometry.add (Primitive.view (the_Primitive));
       end;
 
-
-      return (1 => openGL.Geometry.view (the_Geometry));
+      return (1 => Geometry.view (the_Geometry));
    end to_GL_Geometries;
 
 
--- 3-mid/opengl/source/lean/model/opengl-model-sphere-colored.ads
+++ 3-mid/opengl/source/lean/model/opengl-model-sphere-colored.ads
@@ -1,7 +1,5 @@
 with
-     openGL.Model,
      openGL.Geometry;
-
 
 package openGL.Model.sphere.colored
 --
@@ -9,21 +7,20 @@ package openGL.Model.sphere.colored
 --
 is
 
-   type Item is new openGL.Model.sphere.item with
+   type Item is new openGL.Model.sphere.item with     -- TODO: Make private.
       record
-         Color : openGL.lucid_Color;
+         Color : lucid_Color;
       end record;
 
    type View is access all Item'Class;
-
 
 
    ---------
    --- Forge
    --
 
-   function new_Sphere (Radius : in math.Real;
-                        Color  : in openGL.lucid_Color) return View;
+   function new_Sphere (Radius : in Real;
+                        Color  : in lucid_Color) return View;
 
 
    --------------
@@ -31,7 +28,7 @@ is
    --
 
    overriding
-   function  to_GL_Geometries (Self : access Item;   Textures : access Texture.name_Map_of_texture'Class;
-                                                     Fonts    : in     Font.font_id_Maps_of_font.Map) return openGL.Geometry.views;
+   function to_GL_Geometries (Self : access Item;   Textures : access Texture.name_Map_of_texture'Class;
+                                                    Fonts    : in     Font.font_id_Map_of_font) return Geometry.views;
 
 end openGL.Model.sphere.colored;
