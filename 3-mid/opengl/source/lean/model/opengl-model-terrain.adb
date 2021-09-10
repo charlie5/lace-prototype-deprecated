cc16b7ef0484d138dd09ffb7c538ca2a8474d780
Rod Kay <rodakay5@gmail.com>
2021-09-11 04:26:32 +1000

    opengl.model.terrain: Cosmetics.

--- 3-mid/opengl/source/lean/model/opengl-model-terrain.adb
+++ 3-mid/opengl/source/lean/model/opengl-model-terrain.adb
@@ -7,11 +7,9 @@ with
 
      ada.unchecked_Deallocation;
 
-
 package body openGL.Model.terrain
 is
-   use openGL.Texture;
-
+   use Texture;
 
    --------
    -- Forge
@@ -21,10 +19,10 @@ is
                       Row, Col      : in Integer;
                       Heights       : in height_Map_view;
                       color_Map     : in asset_Name;
-                      Tiling        : in openGL.texture_Transform_2d := (s => (0.0, 1.0),
-                                                                         t => (0.0, 1.0))) return View
+                      Tiling        : in texture_Transform_2d := (S => (0.0, 1.0),
+                                                                  T => (0.0, 1.0))) return View
    is
-      the_Model : constant View := new Item' (openGL.Model.item with
+      the_Model : constant View := new Item' (Model.item with
                                               heights_Asset => heights_Asset,
                                               Heights       => Heights,
                                               Row           => Row,
@@ -41,31 +39,30 @@ is
    overriding
    procedure destroy (Self : in out Item)
    is
-      procedure free is new ada.Unchecked_Deallocation (openGL.height_Map, openGL.Model.terrain.height_Map_view);
+      procedure deallocate is new ada.unchecked_Deallocation (height_Map,
+                                                              height_Map_view);
    begin
-      openGL.Model.destroy (openGL.Model.Item (Self));
-
-      free (self.Heights);
+      destroy (Model.Item (Self));
+      deallocate (Self.Heights);
    end destroy;
-
 
 
    -------------
    -- Attributes
    --
 
-   type Geometry_view is access all openGL.Geometry.lit_colored_textured.item'class;
+   type Geometry_view is access all Geometry.lit_colored_textured.item'Class;
 
 
    overriding
-   function  to_GL_Geometries (Self : access Item;   Textures : access Texture.name_Map_of_texture'Class;
-                                                     Fonts    : in     Font.font_id_Maps_of_font.Map) return openGL.Geometry.views
+   function to_GL_Geometries (Self : access Item;   Textures : access Texture.name_Map_of_texture'Class;
+                                                    Fonts    : in     Font.font_id_Map_of_font) return Geometry.views
    is
-      pragma Unreferenced (Textures, Fonts);
+      pragma unreferenced (Textures, Fonts);
 
-      use openGL.Geometry,
-          openGL.Palette,
-          openGL.Geometry.lit_colored_textured;
+      use Geometry,
+          Palette,
+          Geometry.lit_colored_textured;
 
       Heights       :          height_Map_view renames Self.Heights;
 
@@ -77,40 +74,16 @@ is
       indices_Count : constant long_Index_t :=   (2 * (long_Index_t (Heights'Length (2)) + 1)) * (long_Index_t (row_Count) - 1)
                                                +  2 * (long_Index_t (Heights'Length (2)));
 
-      the_Sites     : aliased  openGL.Sites := (1 .. vertex_Count => <>);
+      the_Sites     : aliased  Sites         := (1 .. vertex_Count => <>);
+      the_Bounds    :          openGL.Bounds := null_Bounds;
 
       the_Vertices  : aliased  Geometry.lit_colored_textured.Vertex_array := (1 .. vertex_Count  => <>);
       the_Indices   : aliased  Indices                                    := (1 .. indices_Count => <>);
 
-      the_Geometry  : constant Geometry_view := Geometry.lit_colored_textured.new_Geometry (texture_is_alpha => False).all'unchecked_Access;
-      the_Bounds    :          openGL.Bounds := null_Bounds;
-
-
---        procedure free is new ada.Unchecked_Deallocationhecked_Deallocation (openGL.height_Map, openGL.IO.height_Map_view);
-
-
---        procedure flip (Self : height_Map_view)
---        is
---           use type openGL.Index_t;
---           Pad : openGL.io.height_Map_view := new openGL.height_Map' (Self.all);
---
---        begin
---           for Row in Self'Range (1)
---           loop
---              for Col in Self'Range (2)
---              loop
---                 Self (Row, Col) := Pad (Self'Last (1) - Row + 1,  Col);
---              end loop;
---           end loop;
---
---           free (Pad);
---        end flip;
-
+      the_Geometry  : constant Geometry_view := Geometry.lit_colored_textured.new_Geometry (texture_is_alpha => False);
 
    begin
---        flip (Heights);
-
-      set_Sites :
+      set_Sites:
       declare
          vert_Id          :          Index_t  := 0;
          the_height_Range : constant Vector_2 := height_Extent (Heights.all);
@@ -128,20 +101,20 @@ is
                                        Heights (flipped_Row, Col) - Middle,
                                        Real (Row)                 - Real (row_Count) / 2.0 - 1.0);
 
-               the_Bounds.Box.Lower (1) := real'Min (the_Bounds.Box.Lower (1),  the_Sites (vert_Id)(1));
-               the_Bounds.Box.Lower (2) := real'Min (the_Bounds.Box.Lower (2),  the_Sites (vert_Id)(2));
-               the_Bounds.Box.Lower (3) := real'Min (the_Bounds.Box.Lower (3),  the_Sites (vert_Id)(3));
+               the_Bounds.Box.Lower (1) := Real'Min (the_Bounds.Box.Lower (1),  the_Sites (vert_Id) (1));
+               the_Bounds.Box.Lower (2) := Real'Min (the_Bounds.Box.Lower (2),  the_Sites (vert_Id) (2));
+               the_Bounds.Box.Lower (3) := Real'Min (the_Bounds.Box.Lower (3),  the_Sites (vert_Id) (3));
 
-               the_Bounds.Box.Upper (1) := real'Max (the_Bounds.Box.Upper (1),  the_Sites (vert_Id)(1));
-               the_Bounds.Box.Upper (2) := real'Max (the_Bounds.Box.Upper (2),  the_Sites (vert_Id)(2));
-               the_Bounds.Box.Upper (3) := real'Max (the_Bounds.Box.Upper (3),  the_Sites (vert_Id)(3));
+               the_Bounds.Box.Upper (1) := Real'Max (the_Bounds.Box.Upper (1),  the_Sites (vert_Id) (1));
+               the_Bounds.Box.Upper (2) := Real'Max (the_Bounds.Box.Upper (2),  the_Sites (vert_Id) (2));
+               the_Bounds.Box.Upper (3) := Real'Max (the_Bounds.Box.Upper (3),  the_Sites (vert_Id) (3));
 
                the_Bounds.Ball := Real'Max (the_Bounds.Ball,
                                             abs (the_Sites (vert_Id)));
 
                the_Vertices (vert_Id).Site  := the_Sites (vert_Id);
-               the_Vertices (vert_Id).Color := (primary => White,
-                                                opacity => openGL.Opaque);
+               the_Vertices (vert_Id).Color := (Primary => White,
+                                                Opacity => Opaque);
             end loop;
          end loop;
 
@@ -149,7 +122,7 @@ is
       end set_Sites;
 
 
-      set_Indices  :
+      set_Indices:
       declare
          Cursor : long_Index_t := 0;
          Start,
@@ -191,56 +164,54 @@ is
       end set_Indices;
 
 
-      set_Normals :
+      set_Normals:
       declare
-         type Normals_view is access all openGL.Normals;
+         type Normals_view is access all Normals;
 
-         the_Normals : Normals_view := openGL.Geometry.Normals_of (Primitive.triangle_Strip,
-                                                                   the_Indices,
-                                                                   the_Sites).all'Access;
-         procedure free is new ada.Unchecked_Deallocation (openGL.Normals, Normals_view);
+         the_Normals : Normals_view := Geometry.Normals_of (Primitive.triangle_Strip,
+                                                            the_Indices,
+                                                            the_Sites);
+         procedure deallocate is new ada.unchecked_Deallocation (Normals,
+                                                                 Normals_view);
 
       begin
-         for Each in the_Vertices'Range
+         for i in the_Vertices'Range
          loop
-            the_Vertices (Each).Normal := the_Normals (Each);
+            the_Vertices (i).Normal := the_Normals (i);
          end loop;
 
-         free (the_Normals);
+         deallocate (the_Normals);
       end set_Normals;
 
 
       if Self.color_Map /= null_Asset
       then
-         set_texture_Coords :
+         set_texture_Coords:
          declare
-            x_Length : constant Real := Real (the_Bounds.Box.upper (1) - the_Bounds.Box.lower (1));
-            x_Min    : constant Real := Real (the_Bounds.Box.lower (1));
+            x_Length : constant Real := the_Bounds.Box.upper (1) - the_Bounds.Box.lower (1);
+            x_Min    : constant Real := the_Bounds.Box.lower (1);
 
---              y_Length : constant Real := Real (the_Bounds.Box.upper (2) - the_Bounds.Box.lower (2));
---              y_Min    : constant Real := Real (the_Bounds.Box.lower (2));
+            z_Length : constant Real := the_Bounds.Box.upper (3) - the_Bounds.Box.lower (3);
+            z_Min    : constant Real := the_Bounds.Box.lower (3);
 
-            z_Length : constant Real := Real (the_Bounds.Box.upper (3) - the_Bounds.Box.lower (3));
-            z_Min    : constant Real := Real (the_Bounds.Box.lower (3));
-
-            upper_Generator : constant openGL.texture.Coordinates.xz_Generator
-              := (normalise => (s => (-x_Min, 1.0/x_Length),
-                                t => (-z_Min, 1.0/z_Length)),
-                  tile      =>  Self.Tiling);
+            upper_Generator : constant Texture.Coordinates.xz_Generator
+              := (Normalise => (S => (-x_Min, 1.0 / x_Length),
+                                T => (-z_Min, 1.0 / z_Length)),
+                  Tile      =>  Self.Tiling);
 
             the_Coords : constant Coordinates_2D := upper_Generator.to_Coordinates (the_Sites'Access);
          begin
-            for Each in the_Coords'Range
+            for i in the_Coords'Range
             loop
-               the_Vertices (Each).Coords := the_Coords (Each);
+               the_Vertices (i).Coords := the_Coords (i);
             end loop;
          end set_texture_Coords;
 
 
-         set_Texture :
+         set_Texture:
          declare
-            the_Image   : constant openGL.Image          := openGL.io.to_Image (Self.color_Map);
-            the_Texture : constant openGL.Texture.object := Forge.to_Texture (the_Image);
+            the_Image   : constant Image          := IO.to_Image (Self.color_Map);
+            the_Texture : constant Texture.object := Forge.to_Texture (the_Image);
          begin
             the_Geometry.Texture_is (the_Texture);
          end set_Texture;
@@ -254,13 +225,13 @@ is
 
       declare
          the_Primitive : constant Primitive.indexed.view
-           := openGL.Primitive.indexed.new_Primitive (primitive.triangle_Strip,
-                                                      the_Indices).all'Access;
+           := Primitive.indexed.new_Primitive (Primitive.triangle_Strip,
+                                               the_Indices);
       begin
-         the_Geometry.add (openGL.Primitive.view (the_Primitive));
+         the_Geometry.add (Primitive.view (the_Primitive));
       end;
 
-      return (1 => the_Geometry.all'Access);
+      return (1 => Geometry.view (the_Geometry));
    end to_GL_Geometries;
 
 
@@ -268,18 +239,18 @@ is
    overriding
    procedure set_Bounds (Self : in out Item)
    is
-      Heights      :          height_Map_view renames Self.Heights;
+      Heights      : height_Map_view renames Self.Heights;
 
-      row_Count    : constant openGL.Index_t  := Heights'Length (1) - 1;
-      col_Count    : constant openGL.Index_t  := Heights'Length (2) - 1;
+      row_Count    : constant Index_t  := Heights'Length (1) - 1;
+      col_Count    : constant Index_t  := Heights'Length (2) - 1;
 
-      vertex_Count : constant openGL.Index_t  := Heights'Length (1) * Heights'Length (2);
+      vertex_Count : constant Index_t  := Heights'Length (1) * Heights'Length (2);
 
-      the_Sites    : aliased  openGL.Sites    := (1 .. vertex_Count => <>);
-      the_Bounds   :          openGL.Bounds   := null_Bounds;
+      the_Sites    : aliased  Sites         := (1 .. vertex_Count => <>);
+      the_Bounds   :          openGL.Bounds := null_Bounds;
 
    begin
-      set_Sites :
+      set_Sites:
       declare
          vert_Id          :          Index_t  := 0;
          the_height_Range : constant Vector_2 := height_Extent (Heights.all);
@@ -296,13 +267,13 @@ is
                                        Heights (Row, Col) - Middle,
                                        Real (Row)         - Real (row_Count) / 2.0 - 1.0);
 
-               the_Bounds.Box.Lower (1) := real'Min (the_Bounds.Box.Lower (1),  the_Sites (vert_Id)(1));
-               the_Bounds.Box.Lower (2) := real'Min (the_Bounds.Box.Lower (2),  the_Sites (vert_Id)(2));
-               the_Bounds.Box.Lower (3) := real'Min (the_Bounds.Box.Lower (3),  the_Sites (vert_Id)(3));
+               the_Bounds.Box.Lower (1) := Real'Min (the_Bounds.Box.Lower (1),  the_Sites (vert_Id) (1));
+               the_Bounds.Box.Lower (2) := Real'Min (the_Bounds.Box.Lower (2),  the_Sites (vert_Id) (2));
+               the_Bounds.Box.Lower (3) := Real'Min (the_Bounds.Box.Lower (3),  the_Sites (vert_Id) (3));
 
-               the_Bounds.Box.Upper (1) := real'Max (the_Bounds.Box.Upper (1),  the_Sites (vert_Id)(1));
-               the_Bounds.Box.Upper (2) := real'Max (the_Bounds.Box.Upper (2),  the_Sites (vert_Id)(2));
-               the_Bounds.Box.Upper (3) := real'Max (the_Bounds.Box.Upper (3),  the_Sites (vert_Id)(3));
+               the_Bounds.Box.Upper (1) := Real'Max (the_Bounds.Box.Upper (1),  the_Sites (vert_Id) (1));
+               the_Bounds.Box.Upper (2) := Real'Max (the_Bounds.Box.Upper (2),  the_Sites (vert_Id) (2));
+               the_Bounds.Box.Upper (3) := Real'Max (the_Bounds.Box.Upper (3),  the_Sites (vert_Id) (3));
 
                the_Bounds.Ball := Real'Max (the_Bounds.Ball,
                                             abs (the_Sites (vert_Id)));
--- 3-mid/opengl/source/lean/model/opengl-model-terrain.ads
+++ 3-mid/opengl/source/lean/model/opengl-model-terrain.ads
@@ -1,25 +1,23 @@
 with
      openGL.Geometry;
 
-
 package openGL.Model.terrain
 --
 --  Models lit, textured terrain.
 --
 is
+   type height_Map_view is access all height_Map;
 
-   type height_Map_view is access all openGL.height_Map;
 
-
-   type Item is new openGL.Model.item with
+   type Item is new Model.item with
       record
-         heights_Asset : asset_Name    := null_Asset;
+         heights_Asset : asset_Name := null_Asset;
 
          Heights       : height_Map_view;
          Row, Col      : Integer;
 
-         color_Map     : asset_Name    := null_Asset;  -- Texture must be square, atm.
-         Tiling        : openGL.texture_Transform_2d;
+         color_Map     : asset_Name := null_Asset;
+         Tiling        : texture_Transform_2D;
       end record;
 
    type View is access all Item'Class;
@@ -33,12 +31,10 @@ is
                       Row, Col      : in Integer;
                       Heights       : in height_Map_view;
                       color_Map     : in asset_Name;
-                      Tiling        : in openGL.texture_Transform_2d :=  (s => (0.0, 1.0),
-                                                                          t => (0.0, 1.0))) return View;
-
+                      Tiling        : in texture_Transform_2d :=  (S => (0.0, 1.0),
+                                                                   T => (0.0, 1.0))) return View;
    overriding
    procedure destroy (Self : in out Item);
-
 
 
    --------------
@@ -46,8 +42,8 @@ is
    --
 
    overriding
-   function  to_GL_Geometries (Self : access Item;   Textures : access Texture.name_Map_of_texture'Class;
-                                                     Fonts    : in     Font.font_id_Maps_of_font.Map) return openGL.Geometry.views;
+   function to_GL_Geometries (Self : access Item;   Textures : access Texture.name_Map_of_texture'Class;
+                                                    Fonts    : in     Font.font_id_Map_of_font) return Geometry.views;
 
 
 private
