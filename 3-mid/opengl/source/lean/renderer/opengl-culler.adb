50d4fd0e36e530e203994f552952ce280c00c357
Rod Kay <rodakay5@gmail.com>
2021-09-11 06:12:01 +1000

    opengl.culler: Cosmetics.

--- 3-mid/opengl/source/lean/renderer/opengl-culler.adb
+++ 3-mid/opengl/source/lean/renderer/opengl-culler.adb
@@ -1,14 +1,14 @@
 package body openGL.Culler
 is
 
-   procedure Viewer_is (Self : in out Item'Class;   Now : in openGL.Renderer.lean.view)
+   procedure Viewer_is (Self : in out Item'Class;   Now : in Renderer.lean.view)
    is
    begin
       Self.Viewer := Now.all'Access;
    end Viewer_is;
 
 
-   function Viewer (Self : in     Item'Class) return openGL.Renderer.lean.view
+   function Viewer (Self : in Item'Class) return Renderer.lean.view
    is
    begin
       return Self.Viewer;
