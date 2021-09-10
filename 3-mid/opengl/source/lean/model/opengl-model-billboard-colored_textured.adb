8861bd5076044d3027efd3f9b12f0531ec76d136
Rod Kay <rodakay5@gmail.com>
2021-09-10 23:43:20 +1000

    opengl.model.billboard.colored_textured: Cosmetics.

--- 3-mid/opengl/source/lean/model/opengl-model-billboard-colored_textured.adb
+++ 3-mid/opengl/source/lean/model/opengl-model-billboard-colored_textured.adb
@@ -1,21 +1,19 @@
 with
      openGL.Primitive.indexed,
-     openGL.io;
-
+     openGL.IO;
 
 package body openGL.Model.billboard.colored_textured
 is
-   type Geometry_view is access all openGL.Geometry.colored_textured.item'Class;
-
+   type Geometry_view is access all Geometry.colored_textured.item'Class;
 
    ---------
    --- Forge
    --
 
-   function new_Billboard (Scale   : in math.Vector_3;
+   function new_Billboard (Scale   : in Vector_3;
                            Plane   : in billboard.Plane;
-                           Color   : in openGL.lucid_Color;
-                           Texture : in openGL.asset_Name) return View
+                           Color   : in lucid_Color;
+                           Texture : in asset_Name) return View
    is
       Self : constant View := new Item;
    begin
@@ -28,60 +26,57 @@ is
    end new_Billboard;
 
 
-
    --------------
    --- Attributes
    --
 
    overriding
    function to_GL_Geometries (Self : access Item;   Textures : access Texture.name_Map_of_texture'Class;
-                                                    Fonts    : in     Font.font_id_Maps_of_font.Map) return openGL.Geometry.views
+                                                    Fonts    : in     Font.font_id_Map_of_font) return Geometry.views
    is
-      pragma Unreferenced (Textures, Fonts);
+      pragma unreferenced (Textures, Fonts);
 
-      use openGL.Geometry,
-          openGL.Geometry.colored_textured,
-          openGL.Texture;
+      use Geometry,
+          Geometry.colored_textured,
+          Texture;
 
       the_Indices  : aliased constant Indices         := (1, 2, 3, 4);
       the_Sites    :         constant billboard.Sites := vertex_Sites (Self.Plane,
-                                                               Self.Scale (1),
-                                                               Self.Scale (2));
+                                                                       Self.Scale (1),
+                                                                       Self.Scale (2));
 
-      function new_Face (Vertices : access openGL.geometry.colored_textured.Vertex_array) return Geometry_view
+      function new_Face (Vertices : access Geometry.colored_textured.Vertex_array) return Geometry_view
       is
-         use
-             openGL.Primitive;
+         use openGL.Primitive;
 
-         the_Geometry  : constant Geometry_view  := openGL.Geometry.colored_textured.new_Geometry.all'unchecked_Access;
+         the_Geometry  : constant Geometry_view  := Geometry.colored_textured.new_Geometry;
          the_Primitive : constant Primitive.view := Primitive.indexed.new_Primitive (triangle_Fan,
                                                                                      the_Indices).all'Access;
       begin
          the_Geometry.Vertices_are (Vertices.all);
-         the_Geometry.add          (the_Primitive);
+         the_Geometry.add (the_Primitive);
          the_Geometry.is_Transparent;
 
          return the_Geometry;
       end new_Face;
 
-
       the_Face : Geometry_view;
 
    begin
       declare
-         the_Vertices : constant access openGL.Geometry.colored_textured.Vertex_array := Self.Vertices;
+         the_Vertices : constant access Geometry.colored_textured.Vertex_array := Self.Vertices;
       begin
-         the_Vertices.all := openGL.Geometry.colored_textured.Vertex_array'
+         the_Vertices.all := Geometry.colored_textured.Vertex_array'
                                (1 => (site => the_Sites (1),    color => Self.Color,  coords => (Self.texture_Coords (1))),
                                 2 => (site => the_Sites (2),    color => Self.Color,  coords => (Self.texture_Coords (2))),
                                 3 => (site => the_Sites (3),    color => Self.Color,  coords => (Self.texture_Coords (3))),
                                 4 => (site => the_Sites (4),    color => Self.Color,  coords => (Self.texture_Coords (4))));
 
-         the_Face         := new_Face (vertices => the_Vertices);
+         the_Face := new_Face (Vertices => the_Vertices);
 
          if Self.texture_Name /= null_Asset
          then
-            Self.Texture := openGL.io.to_Texture (Self.texture_Name);
+            Self.Texture := IO.to_Texture (Self.texture_Name);
          end if;
 
          if Self.Texture /= null_Object
@@ -92,7 +87,7 @@ is
 
       Self.Geometry := the_Face;
 
-      return (1 => the_Face.all'Access);
+      return (1 => Geometry.view (the_Face));
    end to_GL_Geometries;
 
 
@@ -130,7 +125,7 @@ is
 
 
    overriding
-   procedure modify      (Self : in out Item)
+   procedure modify (Self : in out Item)
    is
    begin
       Self.Geometry.Vertices_are (Self.Vertices.all);
@@ -139,7 +134,7 @@ is
 
 
    overriding
-   function  is_Modified (Self : in     Item) return Boolean
+   function is_Modified (Self : in Item) return Boolean
    is
    begin
       return Self.is_Modified;
--- 3-mid/opengl/source/lean/model/opengl-model-billboard-colored_textured.ads
+++ 3-mid/opengl/source/lean/model/opengl-model-billboard-colored_textured.ads
@@ -4,19 +4,16 @@ with
      openGL.Font,
      openGL.Palette;
 
-
 package openGL.Model.billboard.colored_textured
 --
 --  Models a colored, textured billboard.
 --
 is
-
    type Coordinates is array (1 .. 4) of Coordinate_2D;
 
-
-   type Item is new openGL.Model.billboard.item with
+   type Item is new Model.billboard.item with                   -- TODO: Make this private.
       record
-         Color          : lucid_Color           := (openGL.Palette.White, openGL.Opaque);
+         Color          : lucid_Color           := (Palette.White, Opaque);
 
          texture_Name   : asset_Name            := null_Asset;
          Texture        : openGL.Texture.Object := openGL.Texture.null_Object;      -- The texture to be applied to the billboard face.
@@ -24,24 +21,21 @@ is
 
          is_Modified    : Boolean               := False;
 
-         Vertices : access openGL.Geometry.colored_textured.Vertex_array := new openGL.geometry.colored_textured.Vertex_array (1 .. 4);
-         Geometry : access openGL.Geometry.colored_textured.item'Class;
+         Vertices : access Geometry.colored_textured.Vertex_array := new geometry.colored_textured.Vertex_array (1 .. 4);
+         Geometry : access Geometry.colored_textured.item'Class;
       end record;
 
    type View is access all Item'Class;
-
 
 
    ---------
    --- Forge
    --
 
-   function new_Billboard (Scale   : in math.Vector_3;
+   function new_Billboard (Scale   : in Vector_3;
                            Plane   : in billboard.Plane;
-                           Color   : in openGL.lucid_Color;
-                           Texture : in openGL.asset_Name) return View;
-
-
+                           Color   : in lucid_Color;
+                           Texture : in asset_Name) return View;
 
    --------------
    --- Attributes
@@ -49,8 +43,7 @@ is
 
    overriding
    function  to_GL_Geometries   (Self : access Item;   Textures : access Texture.name_Map_of_texture'Class;
-                                                       Fonts    : in     Font.font_id_Maps_of_font.Map) return openGL.Geometry.views;
-
+                                                       Fonts    : in     Font.font_id_Map_of_font) return Geometry.views;
 
    procedure Color_is           (Self : in out Item;   Now : in lucid_Color);
    procedure Texture_Coords_are (Self : in out Item;   Now : in Coordinates);
