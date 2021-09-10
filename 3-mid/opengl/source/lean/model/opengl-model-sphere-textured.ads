35b2cac25cd383c9feb640677a8bed5a1803ef3a
Rod Kay <rodakay5@gmail.com>
2021-09-11 03:58:25 +1000

    opengl.model.sphere.textured: Cosmetics.

--- 3-mid/opengl/source/lean/model/opengl-model-sphere-textured.ads
+++ 3-mid/opengl/source/lean/model/opengl-model-sphere-textured.ads
@@ -1,16 +1,14 @@
 with
-     openGL.Model,
      openGL.Font,
      openGL.Geometry;
 
-
 package openGL.Model.sphere.textured
 --
---  Models a lit, colored, textured sphere.
+--  Models a textured sphere.
 --
 is
 
-   type Item is new openGL.Model.sphere.item with
+   type Item is new Model.sphere.item with     -- TODO: Make private.
       record
          Image        : asset_Name := null_Asset;     -- Usually a mercator projection to be mapped onto the sphere.
          is_Skysphere : Boolean    := False;
@@ -19,22 +17,20 @@ is
    type View is access all Item'Class;
 
 
-
    ---------
    --- Forge
    --
 
-   function new_Sphere (Radius       : in math.Real;
+   function new_Sphere (Radius       : in Real;
                         Image        : in asset_Name := null_Asset;
                         is_Skysphere : in Boolean    := False) return View;
-
 
    --------------
    --- Attributes
    --
 
    overriding
-   function  to_GL_Geometries (Self : access Item;   Textures : access Texture.name_Map_of_texture'Class;
-                                                     Fonts    : in     Font.font_id_Maps_of_font.Map) return openGL.Geometry.views;
+   function to_GL_Geometries (Self : access Item;   Textures : access Texture.name_Map_of_texture'Class;
+                                                    Fonts    : in     Font.font_id_Map_of_font) return Geometry.views;
 
 end openGL.Model.sphere.textured;
