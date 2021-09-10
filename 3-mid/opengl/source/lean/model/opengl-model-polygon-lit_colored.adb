310b01d04458bbc773a62bc76f9f49e2b1aa594e
Rod Kay <rodakay5@gmail.com>
2021-09-11 02:35:22 +1000

    opengl.model.polygon.lit_colored: Cosmetics.

--- 3-mid/opengl/source/lean/model/opengl-model-polygon-lit_colored.adb
+++ 3-mid/opengl/source/lean/model/opengl-model-polygon-lit_colored.adb
@@ -2,7 +2,6 @@ with
      openGL.Geometry.lit_colored,
      openGL.Primitive.indexed;
 
-
 package body openGL.Model.polygon.lit_colored
 is
 
@@ -16,28 +15,28 @@ is
       Self.Vertices (Vertices'Range) := Vertices;
       Self.vertex_Count              := Vertices'Length;
 
-      Self.define  (scale => (1.0, 1.0, 1.0));
+      Self.define (Scale => (1.0, 1.0, 1.0));
       return Self;
    end new_Polygon;
 
 
 
-   type Geometry_view is access all openGL.Geometry.lit_colored.item'class;
+   type Geometry_view is access all Geometry.lit_colored.item'Class;
 
 
-   --  nb: - An extra vertex is required at the end of each latitude ring
+   --  NB: - An extra vertex is required at the end of each latitude ring.
    --      - This last vertex has the same site as the rings initial vertex.
    --      - The  last    vertex has 's' texture coord of 1.0, whereas
-   --        the  initial vertex has 's' texture coord of 0.0
+   --        the  initial vertex has 's' texture coord of 0.0.
    --
    overriding
    function to_GL_Geometries (Self : access Item;   Textures : access Texture.name_Map_of_texture'Class;
-                                                    Fonts    : in     Font.font_id_Maps_of_font.Map) return openGL.Geometry.views
+                                                    Fonts    : in     Font.font_id_Map_of_font) return Geometry.views
    is
-      pragma Unreferenced (Textures, Fonts);
+      pragma unreferenced (Textures, Fonts);
 
-      use openGL.Geometry,
-          openGL.Geometry.lit_colored;
+      use Geometry,
+          Geometry.lit_colored;
 
       vertex_Count  : constant      Index_t :=      Index_t (Self.vertex_Count);
       indices_Count : constant long_Index_t := long_Index_t (Self.vertex_Count);
@@ -45,39 +44,38 @@ is
       the_Vertices  : aliased  Geometry.lit_colored.Vertex_array := (1 .. vertex_Count  => <>);
       the_Indices   : aliased  Indices                           := (1 .. indices_Count => <>);
 
-      the_Geometry  : constant Geometry_view := Geometry_view (Geometry.lit_colored.new_Geometry);
+      the_Geometry  : constant Geometry_view := Geometry.lit_colored.new_Geometry;
 
    begin
-      set_Vertices :
+      set_Vertices:
       begin
-         for Each in 1 .. vertex_Count
+         for i in 1 .. vertex_Count
          loop
-            the_Vertices (Each).Site   := math.Vector_3 (Self.Vertices (Integer (Each)) & 0.0);
-            the_Vertices (Each).Normal := (0.0, 0.0, 1.0);
-            the_Vertices (Each).Color  := Self.Color;
+            the_Vertices (i).Site   := Vector_3 (Self.Vertices (Integer (i)) & 0.0);
+            the_Vertices (i).Normal := (0.0, 0.0, 1.0);
+            the_Vertices (i).Color  := Self.Color;
          end loop;
       end set_Vertices;
 
-
       --- Set Indices.
       --
-      for Each in the_Indices'Range
+      for i in the_Indices'Range
       loop
-         the_Indices (Each) := openGL.Index_t (Each);
+         the_Indices (i) := Index_t (i);
       end loop;
 
       the_Geometry.is_Transparent (False);
       the_Geometry.Vertices_are   (the_Vertices);
 
       declare
-         the_Primitive : constant openGL.Primitive.indexed.view
-           := openGL.Primitive.indexed.new_Primitive (primitive.triangle_Fan,
-                                                      the_Indices);
+         the_Primitive : constant Primitive.indexed.view
+           := Primitive.indexed.new_Primitive (Primitive.triangle_Fan,
+                                               the_Indices);
       begin
-         the_Geometry.add (openGL.Primitive.view (the_Primitive));
+         the_Geometry.add (Primitive.view (the_Primitive));
       end;
 
-      return (1 => openGL.Geometry.view (the_Geometry));
+      return (1 => Geometry.view (the_Geometry));
    end to_GL_Geometries;
 
 
--- 3-mid/opengl/source/lean/model/opengl-model-polygon-lit_colored.ads
+++ 3-mid/opengl/source/lean/model/opengl-model-polygon-lit_colored.ads
@@ -3,14 +3,13 @@ with
      openGL.Font,
      openGL.Geometry;
 
-
 package openGL.Model.polygon.lit_colored
 --
 --  Models a lit, colored polygon.
 --
 is
 
-   type Item is new openGL.Model.polygon.item with
+   type Item is new Model.polygon.item with     -- TODO: Make private.
       record
          Color        : lucid_Color;
 
@@ -27,7 +26,7 @@ is
 
 
    overriding
-   function  to_GL_Geometries (Self : access Item;   Textures : access Texture.name_Map_of_texture'Class;
-                                                     Fonts    : in     Font.font_id_Maps_of_font.Map) return openGL.Geometry.views;
+   function to_GL_Geometries (Self : access Item;   Textures : access Texture.name_Map_of_texture'Class;
+                                                    Fonts    : in     Font.font_id_Map_of_font) return Geometry.views;
 
 end openGL.Model.polygon.lit_colored;
