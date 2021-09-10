da18eead59132f07d38788b7bc5c531175786ae2
Rod Kay <rodakay5@gmail.com>
2021-09-11 00:29:59 +1000

    opengl.model.box.colored: Cosmetics.

--- 3-mid/opengl/source/lean/model/opengl-model-box-colored.adb
+++ 3-mid/opengl/source/lean/model/opengl-model-box-colored.adb
@@ -2,16 +2,16 @@ with
      openGL.Primitive.indexed,
      openGL.Geometry.colored;
 
-
 package body openGL.Model.box.colored
 is
-   type Geometry_view  is access all openGL.Geometry.colored.item'class;
+   type Geometry_view is access all Geometry.colored.item'Class;
+
 
    ---------
    --- Forge
    --
 
-   function new_Box (Size : in math.Vector_3;
+   function new_Box (Size  : in Vector_3;
                      Faces : in colored.Faces) return View
    is
       Self : constant View := new Item;
@@ -23,35 +23,32 @@ is
    end new_Box;
 
 
-
    --------------
    --- Attributes
    --
 
    overriding
    function to_GL_Geometries (Self : access Item;   Textures : access Texture.name_Map_of_texture'Class;
-                                                    Fonts    : in     Font.font_id_Maps_of_font.Map) return openGL.Geometry.views
+                                                    Fonts    : in     Font.font_id_Map_of_font) return Geometry.views
    is
-      pragma Unreferenced (Textures, Fonts);
-
-      use openGL.Geometry;
+      pragma unreferenced (Textures, Fonts);
+      use Geometry;
 
       the_Sites    :         constant box.Sites := Self.vertex_Sites;
       the_Indices  : aliased constant Indices   := (1, 2, 3, 4);
 
 
-      function new_Face (Vertices : access openGL.geometry.colored.Vertex_array) return Geometry_view
+      function new_Face (Vertices : access Geometry.colored.Vertex_array) return Geometry_view
       is
-         use openGL.Geometry.colored,
-             openGL.Primitive;
+         use Geometry.colored,
+             Primitive;
 
-         the_Geometry  : constant Geometry_view          := openGL.Geometry.colored.new_Geometry.all'unchecked_Access;
+         the_Geometry  : constant Geometry_view          := Geometry.colored.new_Geometry;
          the_Primitive : constant Primitive.indexed.view := Primitive.indexed.new_Primitive (triangle_Fan,
-                                                                                             the_Indices).all'Access;
+                                                                                             the_Indices);
       begin
          the_Geometry.Vertices_are   (Vertices.all);
-         the_Geometry.add            (openGL.Primitive.view (the_Primitive));
-
+         the_Geometry.add            (Primitive.view (the_Primitive));
          the_Geometry.is_Transparent (now => False);
 
          return the_Geometry;
@@ -69,82 +66,81 @@ is
       --  Front
       --
       declare
-         the_Vertices : aliased openGL.Geometry.colored.Vertex_array
-           := (1 => (site => the_Sites (left_lower_front),   color => self.Faces (Front).Colors (1)),
-               2 => (site => the_Sites (right_lower_front),  color => self.Faces (Front).Colors (2)),
-               3 => (site => the_Sites (right_upper_front),  color => self.Faces (Front).Colors (3)),
-               4 => (site => the_Sites (left_upper_front),   color => self.Faces (Front).Colors (4)));
+         the_Vertices : aliased Geometry.colored.Vertex_array
+           := (1 => (Site => the_Sites ( Left_Lower_Front),  Color => Self.Faces (Front).Colors (1)),
+               2 => (Site => the_Sites (Right_Lower_Front),  Color => Self.Faces (Front).Colors (2)),
+               3 => (Site => the_Sites (Right_Upper_Front),  Color => Self.Faces (Front).Colors (3)),
+               4 => (Site => the_Sites ( Left_Upper_Front),  Color => Self.Faces (Front).Colors (4)));
       begin
-         front_Face := new_Face (vertices => the_Vertices'Access);
+         front_Face := new_Face (Vertices => the_Vertices'Access);
       end;
 
       --  Rear
       --
       declare
-         the_Vertices : aliased openGL.Geometry.colored.Vertex_array
-           := (1 => (site => the_Sites (Right_Lower_Rear),   color => self.Faces (Rear).Colors (1)),
-               2 => (site => the_Sites (Left_Lower_Rear),    color => self.Faces (Rear).Colors (2)),
-               3 => (site => the_Sites (Left_Upper_Rear),    color => self.Faces (Rear).Colors (3)),
-               4 => (site => the_Sites (Right_Upper_Rear),   color => self.Faces (Rear).Colors (4)));
+         the_Vertices : aliased Geometry.colored.Vertex_array
+           := (1 => (Site => the_Sites (Right_Lower_Rear),   Color => Self.Faces (Rear).Colors (1)),
+               2 => (Site => the_Sites ( Left_Lower_Rear),   Color => Self.Faces (Rear).Colors (2)),
+               3 => (Site => the_Sites ( Left_Upper_Rear),   Color => Self.Faces (Rear).Colors (3)),
+               4 => (Site => the_Sites (Right_Upper_Rear),   Color => Self.Faces (Rear).Colors (4)));
       begin
-         rear_Face := new_Face (vertices => the_Vertices'Access);
+         rear_Face := new_Face (Vertices => the_Vertices'Access);
       end;
 
       --  Upper
       --
       declare
-         the_Vertices : aliased openGL.Geometry.colored.Vertex_array
-           := (1 => (site => the_Sites (Left_Upper_Front),   color => self.Faces (Upper).Colors (1)),
-               2 => (site => the_Sites (Right_Upper_Front),  color => self.Faces (Upper).Colors (2)),
-               3 => (site => the_Sites (Right_Upper_Rear),   color => self.Faces (Upper).Colors (3)),
-               4 => (site => the_Sites (Left_Upper_Rear),    color => self.Faces (Upper).Colors (4)));
+         the_Vertices : aliased Geometry.colored.Vertex_array
+           := (1 => (Site => the_Sites ( Left_Upper_Front),  Color => Self.Faces (Upper).Colors (1)),
+               2 => (Site => the_Sites (Right_Upper_Front),  Color => Self.Faces (Upper).Colors (2)),
+               3 => (Site => the_Sites (Right_Upper_Rear),   Color => Self.Faces (Upper).Colors (3)),
+               4 => (Site => the_Sites ( Left_Upper_Rear),   Color => Self.Faces (Upper).Colors (4)));
       begin
-         upper_Face := new_Face (vertices => the_Vertices'Access);
+         upper_Face := new_Face (Vertices => the_Vertices'Access);
       end;
 
       --  Lower
       --
       declare
-         the_Vertices : aliased openGL.Geometry.colored.Vertex_array
-           := (1 => (site => the_Sites (Right_Lower_Front),   color => self.Faces (Lower).Colors (1)),
-               2 => (site => the_Sites (Left_Lower_Front),    color => self.Faces (Lower).Colors (2)),
-               3 => (site => the_Sites (Left_Lower_Rear),     color => self.Faces (Lower).Colors (3)),
-               4 => (site => the_Sites (Right_Lower_Rear),    color => self.Faces (Lower).Colors (4)));
+         the_Vertices : aliased Geometry.colored.Vertex_array
+           := (1 => (Site => the_Sites (Right_Lower_Front),  Color => Self.Faces (Lower).Colors (1)),
+               2 => (Site => the_Sites ( Left_Lower_Front),  Color => Self.Faces (Lower).Colors (2)),
+               3 => (Site => the_Sites ( Left_Lower_Rear),   Color => Self.Faces (Lower).Colors (3)),
+               4 => (Site => the_Sites (Right_Lower_Rear),   Color => Self.Faces (Lower).Colors (4)));
       begin
-         lower_Face := new_Face (vertices => the_Vertices'Access);
+         lower_Face := new_Face (Vertices => the_Vertices'Access);
       end;
 
       --  Left
       --
       declare
-         the_Vertices : aliased openGL.Geometry.colored.Vertex_array
-           := (1 => (site => the_Sites (Left_Lower_Rear),   color => self.Faces (Left).Colors (1)),
-               2 => (site => the_Sites (Left_Lower_Front),  color => self.Faces (Left).Colors (2)),
-               3 => (site => the_Sites (Left_Upper_Front),  color => self.Faces (Left).Colors (3)),
-               4 => (site => the_Sites (Left_Upper_Rear),   color => self.Faces (Left).Colors (4)));
+         the_Vertices : aliased Geometry.colored.Vertex_array
+           := (1 => (Site => the_Sites (Left_Lower_Rear),    Color => Self.Faces (Left).Colors (1)),
+               2 => (Site => the_Sites (Left_Lower_Front),   Color => Self.Faces (Left).Colors (2)),
+               3 => (Site => the_Sites (Left_Upper_Front),   Color => Self.Faces (Left).Colors (3)),
+               4 => (Site => the_Sites (Left_Upper_Rear),    Color => Self.Faces (Left).Colors (4)));
       begin
-         left_Face := new_Face (vertices => the_Vertices'Access);
+         left_Face := new_Face (Vertices => the_Vertices'Access);
       end;
 
       --  Right
       --
       declare
-         the_Vertices : aliased openGL.Geometry.colored.Vertex_array
-           := (1 => (site => the_Sites (Right_Lower_Front),   color => self.Faces (Right).Colors (1)),
-               2 => (site => the_Sites (Right_Lower_Rear),    color => self.Faces (Right).Colors (2)),
-               3 => (site => the_Sites (Right_Upper_Rear),    color => self.Faces (Right).Colors (3)),
-               4 => (site => the_Sites (Right_Upper_Front),   color => self.Faces (Right).Colors (4)));
+         the_Vertices : aliased Geometry.colored.Vertex_array
+           := (1 => (Site => the_Sites (Right_Lower_Front),  Color => Self.Faces (Right).Colors (1)),
+               2 => (Site => the_Sites (Right_Lower_Rear),   Color => Self.Faces (Right).Colors (2)),
+               3 => (Site => the_Sites (Right_Upper_Rear),   Color => Self.Faces (Right).Colors (3)),
+               4 => (Site => the_Sites (Right_Upper_Front),  Color => Self.Faces (Right).Colors (4)));
       begin
-         right_Face := new_Face (vertices => the_Vertices'Access);
+         right_Face := new_Face (Vertices => the_Vertices'Access);
       end;
 
-
-      return (openGL.Geometry.view (front_Face),
-              openGL.Geometry.view (rear_Face),
-              openGL.Geometry.view (upper_Face),
-              openGL.Geometry.view (lower_Face),
-              openGL.Geometry.view (left_Face),
-              openGL.Geometry.view (right_Face));
+      return (Geometry.view (front_Face),
+              Geometry.view ( rear_Face),
+              Geometry.view (upper_Face),
+              Geometry.view (lower_Face),
+              Geometry.view ( left_Face),
+              Geometry.view (right_Face));
    end to_GL_Geometries;
 
 
--- 3-mid/opengl/source/lean/model/opengl-model-box-colored.ads
+++ 3-mid/opengl/source/lean/model/opengl-model-box-colored.ads
@@ -2,12 +2,11 @@ with
      openGL.Font,
      openGL.Geometry;
 
-
 package openGL.Model.box.colored
 --
 --  Models a colored box.
 --
---  Each side may be separately colored via each of its 4 vertices.
+--  Each face may be separately colored via each of its 4 vertices.
 --
 is
 
@@ -16,7 +15,7 @@ is
 
    type Face is
       record
-         Colors : openGL.lucid_Colors (1 .. 4);  -- The color of each of the faces 4 vertices.
+         Colors : lucid_Colors (1 .. 4);     -- The color of each of the faces 4 vertices.
       end record;
 
    type Faces is array (Side) of Face;
@@ -26,7 +25,7 @@ is
    --- Box
    --
 
-   type Item is new openGL.Model.box.item with
+   type Item is new Model.box.item with     -- TODO: Make private.
       record
          Faces : colored.Faces;
       end record;
@@ -34,14 +33,12 @@ is
    type View is access all Item'Class;
 
 
-
    ---------
    --- Forge
    --
 
-   function new_Box (Size  : in math.Vector_3;
+   function new_Box (Size  : in Vector_3;
                      Faces : in colored.Faces) return View;
-
 
 
    --------------
@@ -49,8 +46,8 @@ is
    --
 
    overriding
-   function  to_GL_Geometries  (Self : access Item;   Textures : access Texture.name_Map_of_texture'Class;
-                                                      Fonts    : in     Font.font_id_Maps_of_font.Map) return openGL.Geometry.views;
+   function to_GL_Geometries (Self : access Item;   Textures : access Texture.name_Map_of_texture'Class;
+                                                    Fonts    : in     Font.font_id_Map_of_font) return Geometry.views;
 
 
 end openGL.Model.box.colored;
