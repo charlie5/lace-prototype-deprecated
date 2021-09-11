df9874f6be3df98b3e61fc2ad099d53f204c4eab
Rod Kay <rodakay5@gmail.com>
2021-09-12 07:02:27 +1000

    opengl.renderer.lean.forge: Cosmetics.

--- 3-mid/opengl/source/lean/renderer/opengl-renderer-lean-forge.adb
+++ 3-mid/opengl/source/lean/renderer/opengl-renderer-lean-forge.adb
@@ -2,10 +2,10 @@ package body openGL.Renderer.lean.forge
 is
 
 
-   function to_Renderer  return openGL.Renderer.lean.item
+   function to_Renderer return Renderer.lean.item
    is
    begin
-      return the_Renderer : openGL.Renderer.lean.item
+      return the_Renderer : Renderer.lean.item
       do
          the_Renderer.define;
       end return;
@@ -13,9 +13,9 @@ is
 
 
 
-   function new_Renderer  return openGL.Renderer.lean.view
+   function new_Renderer return Renderer.lean.view
    is
-      Self : constant openGL.Renderer.lean.view := new openGL.Renderer.lean.item;
+      Self : constant Renderer.lean.view := new Renderer.lean.item;
    begin
       Self.define;
       return Self;
