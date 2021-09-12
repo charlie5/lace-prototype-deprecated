84f42beded5d1bb767853b9f056d779405daa2fb
Rod Kay <rodakay5@gmail.com>
2021-09-13 08:05:07 +1000

    freetype.face: Cosmetics.

--- 3-mid/opengl/private/freetype/source/freetype-face.adb
+++ 3-mid/opengl/private/freetype/source/freetype-face.adb
@@ -1,8 +1,8 @@
 with
-     freetype_C.Binding,
-     freetype_C.FT_Library,
-     freetype_C.FT_Vector,
-     freetype_C.Pointers,
+     freeType_C.Binding,
+     freeType_C.FT_Library,
+     freeType_C.FT_Vector,
+     freeType_C.Pointers,
 
      interfaces.C.Strings,
 
@@ -10,11 +10,8 @@ with
      ada.unchecked_Deallocation,
      ada.Finalization;
 
-
 package body freetype.Face
 is
-
-
    -----------
    --- Globals
    --
@@ -22,14 +19,12 @@ is
    the_FT_Library : aliased FT_Library.item;
 
 
-
    -----------
    --- Utility
    --
 
-   function  to_Flag is new ada.unchecked_Conversion   (FT_Kerning_Mode, C.unsigned);
-   procedure free    is new ada.Unchecked_Deallocation (float_Array,     float_Array_view);
-
+   function  to_Flag    is new ada.unchecked_Conversion   (FT_Kerning_Mode, C.unsigned);
+   procedure deallocate is new ada.Unchecked_Deallocation (float_Array,     float_Array_view);
 
 
    ---------
@@ -41,32 +36,32 @@ is
       function to_Face (fontFilePath      : in String;
                         precomputeKerning : in Boolean) return Face.item
       is
-         use freetype_c.Binding,
-             freetype_c.Pointers,
-             interfaces.C.Strings;
-         use type freetype_c.FT_Long;
+         use freeType_C.Binding,
+             freeType_C.Pointers,
+             C.Strings;
+         use type freeType_C.FT_Long;
 
-         Self          : Face.Item;
+         Self          : Item;
          the_font_Path : chars_ptr := new_String (fontFilePath);
 
       begin
          Self.numGlyphs := 0;
          Self.Err       := 0;
 
-         Self.ftFace := new_FT_Face (the_FT_Library, the_font_Path).all'Access;
+         Self.ftFace := new_FT_Face (the_FT_Library, the_font_Path);
 
          free (the_font_Path);
 
          if Self.ftFace = null
          then
-            raise freetype.Error with "failed to create a freeType Face";
+            raise freetype.Error with "Failed to create a freeType face for '" & fontFilePath & "'.";
          end if;
 
          Self.numGlyphs       := Integer (FT_Face_Get_num_glyphs (Self.ftFace));
          Self.hasKerningTable := FT_Face_HAS_KERNING (Self.ftFace) /= 0;
 
-         if         Self.hasKerningTable
-           and then precomputeKerning
+         if    Self.hasKerningTable
+           and precomputeKerning
          then
             Self.BuildKerningCache;
          end if;
@@ -76,26 +71,26 @@ is
 
 
 
-      function to_Face (pBufferBytes      : access interfaces.c.unsigned_char;
+      function to_Face (pBufferBytes      : access C.unsigned_char;
                         bufferSizeInBytes : in     Positive;
                         precomputeKerning : in     Boolean) return Face.item
       is
-         use freetype_c.Binding,
-             freetype_c.Pointers;
+         use freeType_C.Binding,
+             freeType_C.Pointers;
          use type FT_Long;
 
          Self : Face.item;
 
       begin
          Self.numGlyphs := 0;
-         Self.err       := 0;
+         Self.Err       := 0;
 
          Self.ftFace := new_FT_Memory_Face (the_FT_Library,
                                             pBufferBytes.all'Access,
-                                            C.int (bufferSizeInBytes)).all'Access;
+                                            C.int (bufferSizeInBytes));
          if Self.ftFace = null
          then
-            raise freetype.Error with "failed to create a freeType memory Face";
+            raise freetype.Error with "Failed to create a freeType memory face.";
          end if;
 
          Self.numGlyphs       := Integer (FT_Face_Get_num_glyphs (Self.ftFace));
@@ -114,12 +109,12 @@ is
 
       procedure destruct (Self : in out Item)
       is
-         use freetype_c.Binding;
+         use freeType_C.Binding;
          use type Pointers.FT_FaceRec_Pointer;
       begin
          if Self.kerningCache /= null
          then
-            free (Self.kerningCache);
+            deallocate (Self.kerningCache);
          end if;
 
          if Self.ftFace /= null
@@ -135,8 +130,8 @@ is
 
    function attach (Self : access Item;   fontFilePath : in String) return Boolean
    is
-      use freetype_c.Binding,
-          interfaces.C.Strings;
+      use freeType_C.Binding,
+          C.Strings;
       use type FT_Error;
 
       the_font_Path : chars_ptr := new_String (fontFilePath);
@@ -149,10 +144,10 @@ is
 
 
 
-   function Attach (Self : access Item;   pBufferBytes      : access C.unsigned_char;
+   function attach (Self : access Item;   pBufferBytes      : access C.unsigned_char;
                                           bufferSizeInBytes : in     Positive) return Boolean
    is
-      use freetype_c.Binding;
+      use freeType_C.Binding;
       use type FT_Error;
    begin
       Self.Err := FT_Face_Attach_Stream (Self.ftFace,
@@ -171,15 +166,15 @@ is
 
 
 
-   function Size (Self : access Item;   size         : in Natural;
-                                        x_res, y_res : in Natural) return freetype.face_Size.item
+   function Size (Self : access Item;   Size         : in Natural;
+                                        x_Res, y_Res : in Natural) return freetype.face_Size.item
    is
       Success : Boolean;
-      pragma Unreferenced (Success);
+      pragma unreferenced (Success);
    begin
       Success  := Self.charSize.CharSize (Self.ftFace,
-                                          size,
-                                          x_res, y_res);
+                                          Size,
+                                          x_Res, y_Res);
       Self.Err := Self.charSize.Error;
 
       return Self.charSize;
@@ -189,16 +184,16 @@ is
 
    function CharMapCount (Self : in Item) return Natural
    is
-      use freetype_C.Binding;
+      use freeType_C.Binding;
    begin
       return Natural (FT_Face_Get_num_charmaps (Self.ftFace));
    end CharMapCount;
 
 
 
-   function CharMapList  (Self : access Item) return FT_Encodings_view
+   function CharMapList (Self : access Item) return FT_Encodings_view
    is
-      use freetype_C.Binding;
+      use freeType_C.Binding;
    begin
       if Self.fontEncodingList = null
       then
@@ -207,7 +202,7 @@ is
          for i in 1 .. Self.CharMapCount
          loop
             Self.fontEncodingList (i) := FT_Face_Get_charmap_at (Self.ftFace,
-                                                                 C.int (i)).encoding;
+                                                                 C.int (i)  ).Encoding;
          end loop;
       end if;
 
@@ -219,7 +214,7 @@ is
    function KernAdvance (Self : access Item;   Index1 : in Natural;
                                                Index2 : in Natural) return Vector_3
    is
-      use freetype_c.Binding;
+      use freeType_C.Binding;
       use type FT_Error;
 
       X, Y        :         Float;
@@ -234,14 +229,14 @@ is
       end if;
 
       if    Self.kerningCache /= null
-        and Index1 < MAX_PRECOMPUTED
-        and Index2 < MAX_PRECOMPUTED
+        and Index1 < max_Precomputed     -- TODO: Check this whole function matches C code.
+        and Index2 < max_Precomputed
       then
          declare
-            max_Index : C.ptrdiff_t := C.ptrdiff_t (2 * (Index2 * MAX_PRECOMPUTED + Index1) + 1);
+            max_Index : C.ptrdiff_t := C.ptrdiff_t (2 * (Index2 * max_Precomputed + Index1) + 1);     -- TODO: Check this against C code.
          begin
-            X := Float (Self.kerningCache (C.size_t (2 * (Index2 * MAX_PRECOMPUTED + Index1))));
-            Y := Float (Self.kerningCache (C.size_t (2 * (Index2 * MAX_PRECOMPUTED + Index1) + 1)));
+            X := Float (Self.kerningCache (C.size_t (2 * (Index2 * max_Precomputed + Index1))));
+            Y := Float (Self.kerningCache (C.size_t (2 * (Index2 * max_Precomputed + Index1) + 1)));
 
             return (X, Y, 0.0);
          end;
@@ -253,7 +248,7 @@ is
       Self.Err := FT_Get_Kerning (Self.ftFace,
                                   C.unsigned (index1),
                                   C.unsigned (index2),
-                                  to_Flag    (ft_kerning_unfitted),
+                                  to_Flag    (ft_Kerning_unfitted),
                                   kernAdvance'unchecked_Access);
       if Self.Err /= 0
       then
@@ -277,15 +272,15 @@ is
 
 
    function Glyph (Self : access Item;   Index      : in freetype.charMap.glyphIndex;
-                                         load_Flags : in freetype_c.FT_Int) return FT_GlyphSlot.item
+                                         load_Flags : in freeType_C.FT_Int) return FT_GlyphSlot.item
    is
-      use freetype_C.Binding;
+      use freeType_C.Binding;
       use type FT_Error,
                FT_Face.item;
    begin
       if Self.ftFace = null
       then
-         raise Program_Error;
+         raise freetype.Error with "Face is null.";
       end if;
 
       Self.Err := FT_Load_Glyph (Self.ftFace,  FT_UInt (Index),  load_Flags);
@@ -309,12 +304,12 @@ is
 
    procedure BuildKerningCache (Self : in out Item)
    is
-      use freetype_c.Binding;
+      use freeType_C.Binding;
       use type FT_UInt,
                FT_Error,
                C.C_float;
 
-      max_Index   : constant C.ptrdiff_t   := C.ptrdiff_t (MAX_PRECOMPUTED * MAX_PRECOMPUTED * 2);
+      max_Index   : constant C.ptrdiff_t   := C.ptrdiff_t (max_Precomputed * max_Precomputed * 2);
       kernAdvance : aliased  FT_Vector.item;
 
    begin
@@ -322,22 +317,22 @@ is
       kernAdvance.y     := 0;
       Self.kerningCache := new float_Array' (1 .. C.size_t (max_Index) => <>);
 
-      for j in 1 .. FT_UInt' (MAX_PRECOMPUTED)
+      for j in 1 .. FT_UInt' (max_Precomputed)
       loop
-         for i in 1 .. FT_UInt' (MAX_PRECOMPUTED)
+         for i in 1 .. FT_UInt' (max_Precomputed)
          loop
             Self.Err := FT_Get_Kerning (Self.ftFace,
                                         i, j,
-                                        to_Flag (ft_kerning_unfitted),
-                                        kernAdvance'Unchecked_Access);
+                                        to_Flag (ft_Kerning_unfitted),
+                                        kernAdvance'unchecked_Access);
             if Self.Err /= 0
             then
-               free (Self.kerningCache);
+               deallocate (Self.kerningCache);
                return;
             end if;
 
-            Self.kerningCache (C.size_t (2 * (j * MAX_PRECOMPUTED + i)    )) := C.C_float (kernAdvance.x) / 64.0;
-            Self.kerningCache (C.size_t (2 * (j * MAX_PRECOMPUTED + i) + 1)) := C.C_float (kernAdvance.y) / 64.0;
+            Self.kerningCache (C.size_t (2 * (j * max_Precomputed + i)    )) := C.C_float (kernAdvance.X) / 64.0;
+            Self.kerningCache (C.size_t (2 * (j * max_Precomputed + i) + 1)) := C.C_float (kernAdvance.Y) / 64.0;
          end loop;
       end loop;
 
@@ -348,13 +343,13 @@ is
    -------------------
    --  Package Closure
    --
-   type Closure is new ada.Finalization.Controlled with null record;
+   type Closure is new ada.Finalization.controlled with null record;
 
    overriding
    procedure finalize (Object : in out Closure)
    is
-      use freeType_c.Binding;
-      Status : FT_Error;
+      use freeType_C.Binding;
+      Status : FT_Error with unreferenced;
    begin
       Status := FT_Done_FreeType (the_FT_Library);
    end finalize;
@@ -366,8 +361,8 @@ is
    --------------------------
    --  Package Initialisation
    --
-   use freeType_c.Binding;
-   Status : FT_Error;
+   use freeType_C.Binding;
+   Status : FT_Error with unreferenced;
 
 begin
    Status := FT_init_FreeType (the_FT_Library'Access);
--- 3-mid/opengl/private/freetype/source/freetype-face.ads
+++ 3-mid/opengl/private/freetype/source/freetype-face.ads
@@ -2,10 +2,10 @@ with
      freetype.face_Size,
      freetype.charMap,
 
-     freetype_c.FT_Face,
-     freetype_c.FT_GlyphSlot,
+     freeType_C.FT_Face,
+     freeType_C.FT_GlyphSlot,
 
-     Interfaces.C;
+     interfaces.C;
 
 package freetype.Face
 --
@@ -19,13 +19,14 @@ is
    ---------
    --  Types
    --
-   type FT_Encodings      is array (Positive range <>) of freetype_c.FT_Encoding;
+   type FT_Encodings      is array (Positive range <>) of freeType_C.FT_Encoding;
    type FT_Encodings_view is access all FT_Encodings;
 
 
    ---------
    --  Forge
    --
+   use Interfaces;
 
    package Forge
    is
@@ -34,13 +35,13 @@ is
       --
       --  Opens and reads a face file. Error is set.
 
-      function to_Face (pBufferBytes      : access interfaces.c.unsigned_char;   -- The in-memory buffer.
+      function to_Face (pBufferBytes      : access C.unsigned_char;              -- The in-memory buffer.
                         bufferSizeInBytes : in     Positive;                     -- The length of the buffer in bytes.
                         precomputeKerning : in     Boolean) return Face.item;
       --
       --  Read face data from an in-memory buffer. Error is set.
 
-      procedure destruct (Self : in out Item);   -- Disposes of the current Freetype Face.
+      procedure destruct (Self : in out Item);   -- Disposes of the current Freetype face.
    end Forge;
 
 
@@ -48,67 +49,85 @@ is
    --  Attributes
    --
 
-   function Attach (Self : access Item;   fontFilePath : in String)    -- Auxilliary font file path.
-                    return Boolean;                                    -- Returns true if file has opened successfully.
+   function attach (Self : access Item;   fontFilePath : in String) return Boolean;
    --
    --  Attach auxilliary file to font (e.g., font metrics).
+   --
+   --  fontFilePath: Auxilliary font file path.
+   --
+   --  Returns true if file has opened successfully.
 
-   function Attach (Self : access Item;   pBufferBytes      : access interfaces.c.unsigned_char;
+
+   function attach (Self : access Item;   pBufferBytes      : access C.unsigned_char;
                                           bufferSizeInBytes : in     Positive) return Boolean;
    --
    --  Attach auxilliary data to font (e.g., font metrics) from memory.
+   --
+   --  pBufferBytes:      The in-memory buffer.
+   --  bufferSizeInBytes: The length of the buffer in bytes.
+   --
    --  Returns true if file has opened successfully.
-   --  'pBufferBytes'          The in-memory buffer.
-   --  'bufferSizeInBytes'     The length of the buffer in bytes.
 
-   function freetype_Face (Self : in     Item) return freetype_c.FT_Face.item;
+
+   function freetype_Face (Self : in     Item) return freeType_C.FT_Face.item;
    --
    --  Get the freetype face object.
+   --
    --  Returns a pointer to an FT_Face.
 
-   function Size (Self : access Item;   Size         : in Natural;   -- The face size in points (1/72 inch)
-                                        x_Res, y_Res : in Natural)   -- The resolution of the target device.
-                  return freetype.face_Size.item;
+
+   function Size (Self : access Item;   Size         : in Natural;
+                                        x_Res, y_Res : in Natural) return freetype.face_Size.item;
    --
    --  Sets the char size for the current face.
-   --
    --  This doesn't guarantee that the size was set correctly. Clients should check errors.
+   --
+   --  Size:         The face size in points (1/72 inch).
+   --  x_Res, y_Res: The resolution of the target device.
+   --
    --  Returns FTSize object.
 
-   function CharMapCount (Self : in     Item) return Natural;            -- Return character map count.
+
+   function CharMapCount (Self : in     Item) return Natural;
    --
    --  Get the number of character maps in this face.
+   --
+   -- Return character map count.
+
 
    function CharMapList  (Self : access Item) return FT_Encodings_view;
    --
    --  Get a list of character maps in this face.
+   --
    --  Returns a pointer to the first encoding.
+
 
    function KernAdvance  (Self : access Item;   Index1 : in Natural;
                                                 Index2 : in Natural) return Vector_3;
    --
    --  Gets the kerning vector between two glyphs.
 
+
    function GlyphCount   (Self : in     Item) return Natural;
    --
    --  Gets the number of glyphs in the current face.
 
-   function Glyph (Self : access Item;   Index      : in freetype.charMap.glyphIndex;
-                                         load_Flags : in freetype_c.FT_Int) return freetype_c.FT_GlyphSlot.item;
 
-   function Error (Self : in     Item) return freetype_c.FT_Error;   -- Return the current error code.
+   function Glyph (Self : access Item;   Index      : in freetype.charMap.glyphIndex;
+                                         load_Flags : in freeType_C.FT_Int) return freeType_C.FT_GlyphSlot.item;
+
+   function Error (Self : in     Item) return freeType_C.FT_Error;
    --
-   --  Queries for errors.
+   --  Return the current error code.
 
 
 
 private
 
-   use Freetype_C,
-       Interfaces;
+   use freeType_C;
 
-   type float_Array      is array (C.size_t range <>) of aliased C.c_Float;
-   type float_Array_view is access all float_Array;
+   type Float_array      is array (C.size_t range <>) of aliased C.c_float;
+   type Float_array_view is access all Float_array;
 
 
    type Item is tagged
@@ -118,12 +137,12 @@ private
         numGlyphs        :         Natural;             -- The number of glyphs in this face.
         fontEncodingList :         FT_Encodings_view;
         hasKerningTable  :         Boolean;             -- This face has kerning tables.
-        kerningCache     :         float_Array_view;    -- If this face has kerning tables, we can cache them.
+        kerningCache     :         Float_array_view;    -- If this face has kerning tables, we can cache them.
         Err              :         FT_Error;            -- Current error code. Zero means no error.
       end record;
 
 
-      MAX_PRECOMPUTED : constant := 128;
+      max_Precomputed : constant := 128;
 
       procedure BuildKerningCache (Self : in out Item);
 
