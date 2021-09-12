db756fb4eb44471a40343dede2f38d41dceddf42
Rod Kay <rodakay5@gmail.com>
2021-09-13 06:55:46 +1000

    opengl.glyphimpl.texture: Cosmetics.

--- 3-mid/opengl/source/lean/text/private/opengl-glyphimpl-texture.adb
+++ 3-mid/opengl/source/lean/text/private/opengl-glyphimpl-texture.adb
@@ -1,6 +1,6 @@
 with
-     openGL.Palette,
      openGL.Tasks,
+     openGL.Errors,
 
      GL.Binding,
      GL.Pointers,
@@ -8,23 +8,20 @@ with
      freetype_c.Binding,
      freetype_c.FT_Bitmap,
 
-     Interfaces.C;
+     interfaces.C;
 
-
-package body openGL.GlyphImpl.Texture
+package body openGL.GlyphImpl.texture
 is
-
    -----------
    --  Globals
    --
 
-   activeTextureID : openGL.texture.texture_Name;
+   activeTextureID : openGL.texture.texture_Name;     -- TODO: Check C source for how this is used.
    --
    --  The texture index of the currently active texture
    --
    --  We keep track of the currently active texture to try to reduce the
    --  number of texture bind operations.
-
 
    procedure ResetActiveTexture
    is
@@ -33,37 +30,37 @@ is
    end ResetActiveTexture;
 
 
-
    ---------
    --  Forge
    --
 
    function new_GlyphImpl (glyth_Slot       : in freetype_c.FT_GlyphSlot.item;
-                           texture_id       : in openGL.Texture.texture_Name;
+                           texture_Id       : in openGL.Texture.texture_Name;
                            xOffset, yOffset : in Integer;
-                           width, height    : in Integer) return GlyphImpl.Texture.view
+                           Width,   Height  : in Integer) return GlyphImpl.texture.view
    is
-      use freetype_c,
-          freetype_c.Binding,
+      use freetype_C,
+          freetype_C.Binding,
           GL,
           GL.Binding;
 
-      use type interfaces.c.unsigned,
+      use type interfaces.C.unsigned,
                GLint;
 
-      check_is_OK : constant Boolean                := openGL.Tasks.Check;     pragma Unreferenced (check_is_OK);
-      Self        : constant GlyphImpl.Texture.view := new GlyphImpl.Texture.item;
+      Self : constant GlyphImpl.texture.view := new GlyphImpl.texture.item;
    begin
+      Tasks.check;
+
       Self.define (glyth_Slot);
 
       Self.destWidth   := 0;
       Self.destHeight  := 0;
-      Self.glTextureID := texture_id;
-      Self.err         := error_Kind'Val (FT_Render_Glyph (glyth_Slot,
-                                                           FT_RENDER_MODE_NORMAL));
-      if Self.err /= no_Error
+      Self.glTextureID := texture_Id;
+      Self.Err         := FT_Render_Glyph (glyth_Slot,
+                                           FT_RENDER_MODE_NORMAL);
+      if Self.Err /= no_Error
       then
-         raise openGL.Error with "FT_Render_Glyph failed with error code: " & error_Kind'Image (Self.err);
+         raise openGL.Error with "FT_Render_Glyph failed with error code: " & Self.Err'Image;
       end if;
 
       if FT_GlyphSlot_Get_Format (glyth_Slot) /= get_FT_GLYPH_FORMAT_BITMAP
@@ -73,22 +70,26 @@ is
 
       declare
          use GL.Pointers;
-         bitmap : constant freetype_c.FT_Bitmap.item := FT_GlyphSlot_Get_Bitmap (glyth_Slot);
+         Bitmap : constant freetype_C.FT_Bitmap.item := FT_GlyphSlot_Get_Bitmap (glyth_Slot);
       begin
-         Self.destWidth  := bitmap.width;
-         Self.destHeight := bitmap.rows;
+         Self.destWidth  := Bitmap.Width;
+         Self.destHeight := Bitmap.Rows;
 
          if         Self.destWidth  /= 0
            and then Self.destHeight /= 0
          then
             glPixelStorei   (GL_UNPACK_ALIGNMENT, 1);
 
-            glBindTexture   (GL_TEXTURE_2D,    Self.glTextureID);
+            glBindTexture   (GL_TEXTURE_2D, Self.glTextureID);
+            Errors.log;
+
             glTexSubImage2D (GL_TEXTURE_2D,    0,
                              GLint (xOffset),  GLint (yOffset),
                              Self.destWidth,   Self.destHeight,
                              GL_ALPHA,
-                             GL_UNSIGNED_BYTE, to_GLvoid_access (bitmap.buffer));
+                             GL_UNSIGNED_BYTE,
+                             to_GLvoid_access (Bitmap.Buffer));
+            Errors.log;
          end if;
       end;
 
@@ -100,25 +101,24 @@ is
       --        +----+
       --             1
 
-      Self.uv (1).S := Real (xOffset) / Real (width);
-      Self.uv (1).T := Real (yOffset) / Real (height);
+      Self.UV (1).S := Real (xOffset) / Real (Width);
+      Self.UV (1).T := Real (yOffset) / Real (Height);
 
-      Self.uv (2).S := Real (GLint (xOffset) + Self.destWidth)  / Real (width);
-      Self.uv (2).T := Real (GLint (yOffset) + Self.destHeight) / Real (height);
+      Self.UV (2).S := Real (GLint (xOffset) + Self.destWidth)  / Real (Width);
+      Self.UV (2).T := Real (GLint (yOffset) + Self.destHeight) / Real (Height);
 
-      Self.corner   := (Real (FT_GlyphSlot_Get_bitmap_left (glyth_Slot)),
+      Self.Corner   := (Real (FT_GlyphSlot_Get_bitmap_left (glyth_Slot)),
                         Real (FT_GlyphSlot_Get_bitmap_top  (glyth_Slot)),
                         0.0);
       declare
          use openGL.Primitive;
          the_Indices : constant openGL.Indices := (1, 2, 3, 4);
       begin
-         Self.Primitive := Primitive.indexed.new_Primitive (triangle_Fan,  the_Indices);
+         Self.Primitive := Primitive.indexed.new_Primitive (triangle_Fan, the_Indices);
       end;
 
       return Self;
    end new_GlyphImpl;
-
 
 
    --------------
@@ -127,38 +127,36 @@ is
 
    function Quad (Self : in Item;   Pen : in Vector_3) return Quad_t
    is
-      dx       :         constant Real   := Real'Floor (Pen (1) + Self.corner (1));
-      dy       :         constant Real   := Real'Floor (Pen (2) + Self.corner (2));
+      dx : constant Real := Real'Floor (Pen (1) + Self.Corner (1));
+      dy : constant Real := Real'Floor (Pen (2) + Self.Corner (2));
 
-      the_Quad : aliased constant Quad_t := (NW      => (site   => (dx,
-                                                                    dy,
-                                                                    0.0),
-                                                         coords => (s => Self.uv (1).S,
-                                                                    t => Self.uv (1).T)),
+      the_Quad : aliased constant Quad_t := (NW => (Site   => (dx,
+                                                               dy,
+                                                               0.0),
+                                                    Coords => (S => Self.UV (1).S,
+                                                               T => Self.UV (1).T)),
 
-                                             SW      => (site   => (dx,
-                                                                    dy - Real (Self.destHeight),
-                                                                    0.0),
-                                                         coords => (s => Self.uv (1).S,
-                                                                    t => Self.uv (2).T)),
+                                             SW => (Site   => (dx,
+                                                               dy - Real (Self.destHeight),
+                                                               0.0),
+                                                    Coords => (S => Self.UV (1).S,
+                                                               T => Self.UV (2).T)),
 
-                                             SE      => (site   => (dx + Real (Self.destWidth),
-                                                                    dy - Real (Self.destHeight),
-                                                                    0.0),
-                                                         coords => (s => Self.uv (2).S,
-                                                                    t => Self.uv (2).T)),
+                                             SE => (Site   => (dx + Real (Self.destWidth),
+                                                               dy - Real (Self.destHeight),
+                                                               0.0),
+                                                    Coords => (S => Self.UV (2).S,
+                                                               T => Self.UV (2).T)),
 
-                                             NE      => (site   => (dx + Real (Self.destWidth),
-                                                                    dy,
-                                                                    0.0),
-                                                         coords => (s => Self.uv (2).S,
-                                                                    t => Self.uv (1).T)),
-
-                                             Advance => Self.advance);
+                                             NE => (Site   => (dx + Real (Self.destWidth),
+                                                               dy,
+                                                               0.0),
+                                                    Coords => (S => Self.UV (2).S,
+                                                               T => Self.UV (1).T)),
+                                             Advance => Self.Advance);
    begin
       return the_Quad;
    end Quad;
-
 
 
    --------------
@@ -168,52 +166,9 @@ is
    function renderImpl (Self : in Item;   Pen        : in Vector_3;
                                           renderMode : in Integer) return Vector_3
    is
-      pragma Unreferenced (renderMode);
-
-      dx : constant Real := Real'Floor (Pen (1) + Self.corner (1));
-      dy : constant Real := Real'Floor (Pen (2) + Self.corner (2));
-
+      pragma unreferenced (renderMode);
    begin
-      declare
-         use openGL.Palette;
-
-         the_Vertices : aliased Geometry.lit_colored_textured.Vertex_array
-           := (1 => (site   => (dx,
-                                dy,
-                                0.0),
-                     normal => (0.0, 0.0, 1.0),
-                     color  => (White, Opaque),
-                     coords => (s => Self.uv (1).S,
-                                t => Self.uv (1).T)),
-
-               2 => (site   => (dx,
-                                dy - Real (Self.destHeight),
-                                0.0),
-                     normal => (0.0, 0.0, 1.0),
-                     color  => (White, Opaque),
-                     coords => (s => Self.uv (1).S,
-                                t => Self.uv (2).T)),
-
-               3 => (site   => (dx + Real (Self.destWidth),
-                                dy - Real (Self.destHeight),
-                                0.0),
-                     normal => (0.0, 0.0, 1.0),
-                     color  => (White, Opaque),
-                     coords => (s => Self.uv (2).S,
-                                t => Self.uv (2).T)),
-
-               4 => (site   => (dx + Real (Self.destWidth),
-                                dy,
-                                0.0),
-                     normal => (0.0, 0.0, 1.0),
-                     color  => (White, Opaque),
-                     coords => (s => Self.uv (2).S,
-                                t => Self.uv (1).T)));
-      begin
-         null;
-      end;
-
-      return Self.advance;
+      return Self.Advance;
    end renderImpl;
 
 
--- 3-mid/opengl/source/lean/text/private/opengl-glyphimpl-texture.ads
+++ 3-mid/opengl/source/lean/text/private/opengl-glyphimpl-texture.ads
@@ -8,13 +8,11 @@ with
      openGL.Primitive.indexed,
      GL;
 
-
 package openGL.GlyphImpl.texture
 --
 --  Implements a texture-based glyph.
 --
 is
-
    type Item is new GlyphImpl.item with private;
    type View is access all Item'Class;
 
@@ -29,7 +27,6 @@ is
          Coords : Coordinate_2D;
       end record;
 
-
    type Quad_t is
       record
          NW, NE,
@@ -38,35 +35,38 @@ is
       end record;
 
 
-
    ---------
    --  Forge
    --
-   function new_GlyphImpl (glyth_Slot       : in freetype_c.FT_GlyphSlot.item;   -- The Freetype glyph to be processed.
-                           texture_id       : in openGL.Texture.texture_Name;    -- The id of the texture that this glyph will be drawn in.
-                           xOffset, yOffset : in Integer;                        -- The x,y offset into the parent texture to draw this glyph.
-                           width, height    : in Integer)                        -- The width and height (number of rows) of the parent texture.
-                           return GlyphImpl.Texture.view;
+   function new_GlyphImpl (glyth_Slot       : in freetype_c.FT_GlyphSlot.item;
+                           texture_Id       : in openGL.Texture.texture_Name;
+                           xOffset, yOffset : in Integer;
+                           Width,   Height  : in Integer) return GlyphImpl.texture.view;
+   --
+   --  glyth_Slot:       The Freetype glyph to be processed.
+   --  texture_Id:       The Id of the texture that this glyph will be drawn in.
+   --  xOffset, yOffset: The x any y offset into the parent texture to draw this glyph.
+   --  Width,   Height:  The width and height (number of rows) of the parent texture.
 
 
    --------------
    --  Attributes
    --
 
-   function Quad       (Self : in     Item;   Pen        : in Vector_3) return Quad_t;
-
+   function Quad (Self : in Item;   Pen : in Vector_3) return Quad_t;
 
 
    --------------
    --  Operations
    --
 
-   function renderImpl (Self : in     Item;   Pen        : in Vector_3;   -- The current pen position.
-                                              renderMode : in Integer)    -- Render mode to display.
-                        return Vector_3;
+   function renderImpl (Self : in Item;   Pen        : in Vector_3;
+                                          renderMode : in Integer) return Vector_3;
+   --
+   --  Pen:         The current pen position.
+   --  renderMode:  Render mode to display.
    --
    --  Returns the advance distance for this glyph.
-
 
 
    -------------
@@ -76,7 +76,7 @@ is
    procedure ResetActiveTexture;
    --
    --  Reset the currently active texture to zero to get into a known
-   --  state before drawing a string. This is to get round possible threading issues.
+   --  state before drawing a string. This is to get around possible threading issues.
 
 
 
@@ -87,14 +87,13 @@ private
          destWidth,                                             -- The width and height of the glyph 'image'.
          destHeight  :        GL.GLint;
 
-         corner      :        Vector_3;                         -- Vector from the pen site to the top left of the pixmap.
+         Corner      :        Vector_3;                         -- Vector from the pen site to the top left of the pixmap.
 
-         uv          :        openGL.Coordinates_2D (1 .. 2);   -- The texture co-ords of this glyph within the texture.
+         UV          :        Coordinates_2D (1 .. 2);          -- The texture co-ords of this glyph within the texture.
          glTextureID :        openGL.texture.texture_Name;      -- The texture index that this glyph is contained in.
 
-         Geometry    : access openGL.Geometry.lit_colored_textured.item;
+         Geometry    : access Geometry.lit_colored_textured.item;
          Primitive   :        openGL.Primitive.indexed.view;
-
       end record;
 
 end openGL.GlyphImpl.texture;
