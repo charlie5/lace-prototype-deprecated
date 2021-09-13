06e4879ea73d11fed8f46b43ecc8633e3d3dad10
Rod Kay <rodakay5@gmail.com>
2021-09-14 07:45:14 +1000

    physics.remote.model: Cosmetics.

--- 3-mid/physics/interface/source/physics-remote-model.ads
+++ 3-mid/physics/interface/source/physics-remote-model.ads
@@ -1,15 +1,14 @@
 package physics.remote.Model
 --
---  A model describing physical properties, usable by DSA.
+--  A model describing physical properties, usable with DSA.
 --
 is
-   pragma Remote_Types;
-
+   pragma remote_Types;
 
    type Item is abstract tagged
       record
-         Id    : physics.model_Id := null_model_Id;
-         Scale : math.Vector_3        := (1.0, 1.0, 1.0);
+         Id    : model_Id := null_model_Id;
+         Scale : Vector_3 := (1.0, 1.0, 1.0);
       end record;
 
 end physics.remote.Model;
