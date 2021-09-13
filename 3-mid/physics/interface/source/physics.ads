ac921f263b081a645ca8e5c37fda959b6d8e89ed
Rod Kay <rodakay5@gmail.com>
2021-09-14 06:19:26 +1000

    physics: Cosmetics.

--- 3-mid/physics/interface/source/physics.ads
+++ 3-mid/physics/interface/source/physics.ads
@@ -1,19 +1,18 @@
 with
-     float_Math.Geometry.d2,
-     float_Math.Geometry.d3,
-     float_Math.Algebra.linear.d3;
+     float_Math.Geometry.D2,
+     float_Math.Geometry.D3,
+     float_Math.Algebra.linear.D3;
 
 package Physics
 --
---  Provides a physics interface for 2/3d simulations.
+--  Provides a physics interface for 2D/3D simulations.
 --
 is
    pragma Pure;
 
-
-   package Math        renames float_Math;
-   package Geometry_2D renames math.Geometry.d2;
-   package Geometry_3D renames math.Geometry.d3;
+   package Math              renames float_Math;
+   package Geometry_2D       renames math.Geometry.d2;
+   package Geometry_3D       renames math.Geometry.d3;
    package linear_Algebra_3D renames math.Algebra.linear.d3;
 
    type Vector_2_array is array (Positive range <>) of math.Vector_2;
@@ -23,19 +22,17 @@ is
                                  Positive range <>) of aliased math.Real;
 
 
+   type space_Kind is (Bullet, Box2D);
 
-   type space_Kind is (Bullet,    Box2d);
-
-
-   max_physics_Models  : constant := 2**32 - 1;
+   max_physics_Models : constant := 2**32 - 1;
    type model_Id is range 0 .. max_physics_Models;
 
-   null_physics_model_Id  : constant physics.model_Id;
+   null_physics_model_Id : constant physics.model_Id;
 
 
    unsupported_Error : exception;
    --
-   -- Raised when a shape, joint is not supported in a space.
+   -- Raised when a shape or joint is not supported in a space.
 
 
 private
