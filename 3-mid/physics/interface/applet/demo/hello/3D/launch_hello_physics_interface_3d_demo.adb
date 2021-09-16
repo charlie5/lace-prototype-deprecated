6c63ff99e764145315241c3d8197a57725ac8fa5
Rod Kay <rodakay5@gmail.com>
2021-09-17 08:24:41 +1000

    physics.demo.hello.3d: Add new demo.

---
+++ 3-mid/physics/interface/applet/demo/hello/3D/launch_hello_physics_interface_3d_demo.adb
@@ -0,0 +1,47 @@
+with
+     physics.Space,
+     physics.Shape,
+     physics.Object,
+     physics.Forge,
+
+     ada.Text_IO;
+
+procedure launch_hello_physics_interface_3D_Demo
+--
+-- Drops a ball onto a box.
+--
+is
+   use physics.Math,
+       physics.Forge,
+       ada.Text_IO;
+
+   the_Space  : constant physics.Space .view := new_Space (physics.Bullet);
+
+   the_Sphere : constant physics.Shape .view := the_Space.new_sphere_Shape;
+   the_Box    : constant physics.Shape .view := the_Space.new_box_Shape (half_Extents => (1.0, 1.0, 1.0));
+
+   the_Ball   : constant physics.Object.view := the_Space.new_Object (of_Shape     => the_Sphere,
+                                                                      of_Mass      => 1.0,
+                                                                      Friction     => 0.5,
+                                                                      Restitution  => 0.5,
+                                                                      at_Site      => (0.0,  10.0, 0.0),
+                                                                      is_Kinematic => False);
+
+   the_Ground : constant physics.Object.view := the_Space.new_Object (of_Shape     => the_Box,
+                                                                      of_Mass      => 0.0,
+                                                                      Friction     => 0.5,
+                                                                      Restitution  => 0.5,
+                                                                      at_Site      => (0.0, -1.5, 0.0),
+                                                                      is_Kinematic => False);
+begin
+   the_Space.add (the_Ball);
+   the_Space.add (the_Ground);
+
+   for i in 1 .. 150
+   loop
+      the_Space.evolve (by => 1.0/60.0);
+
+      put_Line (  "Sites ~ Ball => "   & Image (the_Ball  .Site)
+                & "        Ground => " & Image (the_Ground.Site));
+   end loop;
+end launch_hello_physics_interface_3D_Demo;
