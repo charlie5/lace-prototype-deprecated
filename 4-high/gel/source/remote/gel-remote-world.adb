f154d729f808ddc65600eef037c831c3e49fe992
Rod Kay <rodakay5@gmail.com>
2021-09-19 13:11:06 +1000

    gel.remote.world: Cosmetics.

--- 4-high/gel/source/remote/gel-remote-world.adb
+++ 4-high/gel/source/remote/gel-remote-world.adb
@@ -11,15 +11,14 @@ is
 
 
 
-
-   function coarsen (Self : in   math.Vector_3) return coarse_Vector_3
+   function coarsen (Self : in math.Vector_3) return coarse_Vector_3
    is
       Result : coarse_Vector_3;
    begin
       begin
          Result (1) := coarse_Real (Self (1));
       exception
-         when Constraint_Error =>
+         when constraint_Error =>
             if Self (1) > 0.0 then
                Result (1) := coarse_Real'Last;
             else
@@ -30,7 +29,7 @@ is
       begin
          Result (2) := coarse_Real (Self (2));
       exception
-         when Constraint_Error =>
+         when constraint_Error =>
             if Self (2) > 0.0 then
                Result (2) := coarse_Real'Last;
             else
@@ -41,7 +40,7 @@ is
       begin
          Result (3) := coarse_Real (Self (3));
       exception
-         when Constraint_Error =>
+         when constraint_Error =>
             if Self (3) > 0.0 then
                Result (3) := coarse_Real'Last;
             else
@@ -57,22 +56,22 @@ is
    function refined (Self : in coarse_Quaternion) return math.Quaternion
    is
    begin
-      return (math.Real (Self (1)),
-              (math.Real (Self (2)),
-               math.Real (Self (3)),
-               math.Real (Self (4))));
+      return (R =>  math.Real (Self (1)),
+              V => (math.Real (Self (2)),
+                    math.Real (Self (3)),
+                    math.Real (Self (4))));
    end refined;
 
 
 
-   function coarsen (Self : in   math.Quaternion) return coarse_Quaternion
+   function coarsen (Self : in math.Quaternion) return coarse_Quaternion
    is
       Result : coarse_Quaternion;
    begin
       begin
          Result (1) := coarse_Real2 (Self.R);
       exception
-         when Constraint_Error =>
+         when constraint_Error =>
             if Self.R > 0.0 then
                Result (1) := coarse_Real2'Last;
             else
@@ -83,7 +82,7 @@ is
       begin
          Result (2) := coarse_Real2 (Self.V (1));
       exception
-         when Constraint_Error =>
+         when constraint_Error =>
             if Self.V (1) > 0.0 then
                Result (2) := coarse_Real2'Last;
             else
@@ -94,7 +93,7 @@ is
       begin
          Result (3) := coarse_Real2 (Self.V (2));
       exception
-         when Constraint_Error =>
+         when constraint_Error =>
             if Self.V (2) > 0.0 then
                Result (3) := coarse_Real2'Last;
             else
@@ -117,10 +116,8 @@ is
    end coarsen;
 
 
-
-
    -----------
-   --  Streams
+   --- Streams
    --
 
    use ada.Streams;
@@ -129,15 +126,14 @@ is
      := motion_Update'Size / Stream_Element'Size;
 
 
-
-   procedure motion_Updates_write (Stream : access Ada.Streams.Root_Stream_Type'Class;   Item : in  motion_Updates)
+   procedure motion_Updates_write (Stream : access ada.Streams.Root_Stream_type'Class;   Item : in  motion_Updates)
    is
       stream_element_array_Length : constant Stream_Element_Offset
         := Item'Length * number_of_stream_Elements_for_a_Motion_Update;
 
       subtype the_Stream_Element_Array  is Stream_Element_Array (1 .. stream_element_array_Length);
 
-      function to_Stream_Element_Array is new ada.Unchecked_Conversion (motion_Updates, the_Stream_Element_Array);
+      function to_Stream_Element_Array is new ada.unchecked_Conversion (motion_Updates, the_Stream_Element_Array);
 
    begin
       write (Stream.all, to_Stream_Element_Array (Item));
@@ -145,14 +141,14 @@ is
 
 
 
-   procedure motion_Updates_read (Stream : access Ada.Streams.Root_Stream_Type'Class;    Item : out motion_Updates)
+   procedure motion_Updates_read (Stream : access ada.Streams.Root_Stream_type'Class;    Item : out motion_Updates)
    is
       subtype the_Stream_Element_Array
         is Stream_Element_Array (1 .. Item'Length * number_of_stream_Elements_for_a_motion_Update);
 
-      subtype the_motion_Updates       is motion_Updates       (1 .. Item'Length);
+      subtype the_motion_Updates is motion_Updates (1 .. Item'Length);
 
-      function to_motion_Updates is new ada.Unchecked_Conversion (the_Stream_Element_Array, the_motion_Updates);
+      function to_motion_Updates is new ada.unchecked_Conversion (the_Stream_Element_Array, the_motion_Updates);
 
       the_Stream_Array : the_Stream_Element_Array;
       Last             : Stream_Element_Offset;
@@ -160,24 +156,24 @@ is
    begin
       read (Stream.all, the_Stream_Array, Last);
 
-      pragma Assert (Last = the_Stream_Array'Last);
+      pragma assert (Last = the_Stream_Array'Last);
 
       Item := to_motion_Updates (the_Stream_Array (1 .. Last));
    end motion_Updates_read;
 
 
 
-
-   procedure Write (Stream    : not null access ada.Streams.Root_Stream_Type'Class;
+   procedure Write (Stream    : not null access ada.Streams.Root_Stream_type'Class;
                     the_Event : in              new_model_Event)
    is
    begin
       openGL.remote_Model.item'Class'Output (Stream,
-                                              the_Event.Model.all);
+                                             the_Event.Model.all);
    end Write;
 
 
-   procedure Read (Stream    : not null access ada.Streams.Root_Stream_Type'Class;
+
+   procedure Read (Stream    : not null access ada.Streams.Root_Stream_type'Class;
                    the_Event : out             new_model_Event)
    is
    begin
@@ -186,7 +182,7 @@ is
 
 
 
-   procedure Write (Stream    : not null access ada.Streams.Root_Stream_Type'Class;
+   procedure Write (Stream    : not null access ada.Streams.Root_Stream_type'Class;
                     the_Event : in              new_physics_model_Event)
    is
    begin
@@ -194,7 +190,7 @@ is
    end Write;
 
 
-   procedure Read (Stream    : not null access ada.Streams.Root_Stream_Type'Class;
+   procedure Read (Stream    : not null access ada.Streams.Root_Stream_type'Class;
                    the_Event : out             new_physics_model_Event)
    is
    begin
--- 4-high/gel/source/remote/gel-remote-world.ads
+++ 4-high/gel/source/remote/gel-remote-world.ads
@@ -11,25 +11,20 @@ with
      ada.Containers.indefinite_Vectors,
      ada.Streams;
 
-
 package gel.remote.World
 --
---  Provides a remote interface of an gel world.
+--  Provides a remote (DSA friendly) interface of a GEL world.
 --
 --  Supports world mirroring, in which a mirror world mimics the objects and dynamics of a master world.
 --
 is
-   pragma Remote_Types;
-
+   pragma remote_Types;
 
    type Item is  limited interface
              and lace.Subject .item
              and lace.Observer.item;
 
-   type View is access all Item'Class;
-
-   pragma Asynchronous (View);
-
+   type View is access all Item'Class with asynchronous;
 
 
    -----------
@@ -44,26 +39,26 @@ is
    procedure deregister (Self : access Item;   the_Mirror         : in World.view)           is abstract;
 
 
-
    ----------
    --  Models
    --
 
    --  Graphics
    --
-   use type openGL.remote_Model.item;
-   package  model_Vectors is new ada.containers.indefinite_Vectors (Positive,  openGL.remote_Model.item'Class);
 
-   function Hash is new ada.unchecked_Conversion (gel.graphics_model_Id,  ada.containers.Hash_type);
+   use type openGL.remote_Model.item;
+   package  model_Vectors is new ada.Containers.indefinite_Vectors (Positive, openGL.remote_Model.item'Class);
+
+   function Hash is new ada.unchecked_Conversion (gel.graphics_model_Id, ada.containers.Hash_type);
    use type gel.graphics_model_Id;
 
-   package  Id_Maps_of_Model_Plan is new ada.containers.indefinite_Hashed_Maps (gel.graphics_model_Id,
+   package  id_Maps_of_model_plan is new ada.Containers.indefinite_Hashed_Maps (gel.graphics_model_Id,
                                                                                 openGL.remote_Model.item'Class,
                                                                                 Hash,
                                                                                 "=");
-   subtype  graphics_Model_Set is Id_Maps_of_Model_Plan.Map;
+   subtype  graphics_Model_Set is id_Maps_of_model_plan.Map;     -- TODO: Rename to id_Map_of_graphics_model_plan.
 
-   function graphics_Models (Self : in Item) return graphics_Model_Set   is abstract;
+   function graphics_Models (Self : in Item) return graphics_Model_Set is abstract;
 
 
    type new_model_Event is new lace.Event.item with
@@ -72,25 +67,27 @@ is
       end record;
 
 
-   procedure Write (Stream : not null access ada.Streams.Root_Stream_Type'Class;   the_Event : in  new_model_Event);
-   procedure Read  (Stream : not null access ada.Streams.Root_Stream_Type'Class;   the_Event : out new_model_Event);
+   procedure Write (Stream : not null access ada.Streams.Root_Stream_type'Class;   the_Event : in  new_model_Event);
+   procedure Read  (Stream : not null access ada.Streams.Root_Stream_type'Class;   the_Event : out new_model_Event);
 
-   for new_model_Event'Write use Write;
-   for new_model_Event'Read  use Read;
+   for new_model_Event'write use write;
+   for new_model_Event'read  use read;
 
 
    --  Physics
    --
+
    use physics.remote.Model;
-   package  physics_model_Vectors is new ada.containers.indefinite_Vectors (Positive,  physics.remote.Model.item'Class);
+   package  physics_model_Vectors is new ada.containers.indefinite_Vectors (Positive, physics.remote.Model.item'Class);
 
    use type physics.model_Id;
    function Hash is new ada.unchecked_Conversion (physics.model_Id, ada.containers.Hash_type);
-   package  Id_Maps_of_physics_Model_Plan is new ada.containers.indefinite_Hashed_Maps (physics.model_Id,
+
+   package  id_Maps_of_physics_model_plan is new ada.containers.indefinite_Hashed_Maps (physics.model_Id,
                                                                                         physics.remote.Model.item'Class,
                                                                                         Hash,
                                                                                         "=");
-   subtype  physics_Model_Set is Id_Maps_of_physics_Model_Plan.Map;
+   subtype  physics_Model_Set is id_Maps_of_physics_model_plan.Map;     -- TODO: Rename to id_Map_of_physics_model_plan.
 
    function physics_Models (Self : in Item) return physics_Model_Set is abstract;
 
@@ -100,23 +97,21 @@ is
          Model : access physics.remote.Model.item'Class;
       end record;
 
+   procedure Write (Stream : not null access ada.Streams.Root_Stream_type'Class;   the_Event : in  new_physics_model_Event);
+   procedure Read  (Stream : not null access ada.Streams.Root_Stream_type'Class;   the_Event : out new_physics_model_Event);
 
-   procedure Write (Stream : not null access ada.Streams.Root_Stream_Type'Class;   the_Event : in  new_physics_model_Event);
-   procedure Read  (Stream : not null access ada.Streams.Root_Stream_Type'Class;   the_Event : out new_physics_model_Event);
-
-   for new_physics_model_Event'Write use Write;
-   for new_physics_model_Event'Read  use Read;
-
+   for new_physics_model_Event'write use write;
+   for new_physics_model_Event'read  use read;
 
 
    -----------
-   --  Sprites
+   --- Sprites
    --
 
    type sprite_model_Pair is
       record
-         sprite_Id         : gel.sprite_Id;
-         graphics_model_Id : openGL.model_Id;
+         sprite_Id         : gel   .sprite_Id;
+         graphics_model_Id : openGL .model_Id;
          physics_model_Id  : physics.model_Id;
 
          Mass              : math.Real;
@@ -126,17 +121,16 @@ is
 
    type sprite_model_Pairs is array (math.Index range <>) of sprite_model_Pair;
 
-   function Sprites (Self : in Item) return sprite_model_Pairs   is abstract;
-
+   function Sprites (Self : in Item) return sprite_model_Pairs is abstract;
 
 
    -------------------------
-   --  Sprite Motion Updates
+   --- Sprite Motion Updates
    --
 
-   --  Coarse types to help minimise network use - (tbd: currently disabled til better quaternion 'coarsen' is ready)
+   --  Coarse types to help minimise network use - (TODO: Currently disabled til better quaternion 'coarsen' is ready.)
    --
-   type coarse_Real is new math.Real;   -- not ccoarse atm (see above 'tbd')
+   type coarse_Real is new math.Real;   -- Not coarse atm (see above 'TODO')
 
    type coarse_Vector_3 is array (1 .. 3) of coarse_Real;
 
@@ -144,7 +138,7 @@ is
    function coarsen (Self : in   math.Vector_3) return coarse_Vector_3;
 
 
-   type coarse_Real2 is new math.Real;   -- not coarse atm
+   type coarse_Real2 is new math.Real;   -- Not coarse atm.
 
 
    type coarse_Quaternion is array (1 .. 4) of coarse_Real2;
@@ -153,28 +147,25 @@ is
    function coarsen (Self : in   math.Quaternion) return coarse_Quaternion;
 
 
-
    type motion_Update is
       record
          Id   : gel.sprite_Id;
          Site : coarse_Vector_3;
          Spin : coarse_Quaternion;
-      end record;
-   pragma Pack (motion_Update);
+      end record
+   with Pack;
 
 
-   type motion_Updates is array (Positive range <>) of motion_Update;
-   pragma Pack (motion_Updates);
+   type motion_Updates is array (Positive range <>) of motion_Update
+   with Pack;
 
+   procedure motion_Updates_write (Stream : access ada.Streams.Root_Stream_type'Class;   Item : in  motion_Updates);
+   procedure motion_Updates_read  (Stream : access ada.Streams.Root_Stream_type'Class;   Item : out motion_Updates);
 
-   procedure motion_Updates_write (Stream : access Ada.Streams.Root_Stream_Type'Class;   Item : in  motion_Updates);
-   procedure motion_Updates_read  (Stream : access Ada.Streams.Root_Stream_Type'Class;   Item : out motion_Updates);
+   for motion_Updates'write use motion_Updates_write;
+   for motion_Updates'read  use motion_Updates_read;
 
-   for motion_Updates'Write use motion_Updates_write;
-   for motion_Updates'Read  use motion_Updates_read;
-
-   procedure motion_Updates_are (Self : in Item;   Now : in motion_Updates)   is abstract;
-
+   procedure motion_Updates_are (Self : in Item;   Now : in motion_Updates) is abstract;
 
 
    --------------
