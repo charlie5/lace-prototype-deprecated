8995f0b1009a87b5a4929715ed754dd2eb5962da
Rod Kay <rodakay5@gmail.com>
2021-09-24 11:13:42 +1000

    gel.demo.drop_box_on_box: Cosmetics.

--- 4-high/gel/applet/demo/sprite/drop_box_on_box/launch_drop_box_on_box.adb
+++ 4-high/gel/applet/demo/sprite/drop_box_on_box/launch_drop_box_on_box.adb
@@ -9,10 +9,10 @@ with
      openGL.Model.box.colored,
      openGL.Palette,
 
-     Ada.Text_IO,
-     Ada.Exceptions;
+     ada.Text_IO,
+     ada.Exceptions;
 
-pragma Unreferenced (gel.window.lumen);
+pragma unreferenced (gel.window.lumen);
 
 procedure launch_drop_Box_on_Box
 --
@@ -21,49 +21,49 @@ procedure launch_drop_Box_on_Box
 is
    use gel.Applet,    openGL.Model.box,
        openGL,        opengl.Palette,
-       Ada.Text_IO;
+       ada.Text_IO;
 
    the_Applet : constant gel.Applet.gui_World.view
      := gel.Forge.new_gui_Applet ("drop Ball on Box");
 
 
    the_ground_graphics_Model : constant openGL.Model.box.colored.view
-     := openGL.Model.box.colored.new_Box (size => (10.0, 0.5, 10.0),
-                                                      faces => (front => (colors => (others => (Red,     Opaque))),
-                                                                rear  => (colors => (others => (Blue,    Opaque))),
-                                                                upper => (colors => (others => (Green,   Opaque))),
-                                                                lower => (colors => (others => (Yellow,  Opaque))),
-                                                                left  => (colors => (others => (Cyan,    Opaque))),
-                                                                right => (colors => (others => (Magenta, Opaque)))));
+     := openGL.Model.box.colored.new_Box (Size  => (10.0, 0.5, 10.0),
+                                          Faces => (Front => (Colors => (others => (Red,     Opaque))),
+                                                    Rear  => (Colors => (others => (Blue,    Opaque))),
+                                                    Upper => (Colors => (others => (Green,   Opaque))),
+                                                    Lower => (Colors => (others => (Yellow,  Opaque))),
+                                                    Left  => (Colors => (others => (Cyan,    Opaque))),
+                                                    Right => (Colors => (others => (Magenta, Opaque)))));
    the_ground_physics_Model : constant physics.Model.view
-     := physics.Model.Forge.new_physics_Model (shape_Info => (kind         => physics.Model.Cube,
-                                                              half_extents => (5.0, 0.25, 5.0)));
+     := physics.Model.Forge.new_physics_Model (shape_Info => (Kind         => physics.Model.cube,
+                                                              half_Extents => (5.0, 0.25, 5.0)));
 
    the_Ground : constant gel.Sprite.view
      := gel.Sprite.forge.new_Sprite ("demo.Ground",
                                      gel.Sprite.World_view (the_Applet.gui_World),
-                                     math.Origin_3d,
-                                     the_ground_graphics_Model.all'access,
+                                     math.Origin_3D,
+                                     the_ground_graphics_Model,
                                      the_ground_physics_Model);
 
 
    the_box_physics_Model : constant physics.Model.view
-     := physics.Model.Forge.new_physics_Model (shape_Info => (kind        => physics.Model.cube,
-                                                              half_extents => (1.0, 0.5, 1.0)),
-                                                   Mass => 1.0);
+     := physics.Model.forge.new_physics_Model (shape_Info => (Kind        => physics.Model.cube,
+                                                              half_Extents => (1.0, 0.5, 1.0)),
+                                               Mass => 1.0);
 
    the_box_graphics_Model : constant openGL.Model.box.colored.view
-     := openGL.Model.box.colored.new_Box (size => (1.0, 1.0, 1.0),
-                                          faces => (front => (colors => (others => (Red,        Opaque))),
-                                                    rear  => (colors => (others => (Blue,       Opaque))),
-                                                    upper => (colors => (others => (dark_Green, Opaque))),
-                                                    lower => (colors => (others => (Yellow,     Opaque))),
-                                                    left  => (colors => (others => (Cyan,       Opaque))),
-                                                    right => (colors => (others => (Magenta,    Opaque)))));
+     := openGL.Model.box.colored.new_Box (Size => (1.0, 1.0, 1.0),
+                                          Faces => (Front => (Colors => (others => (Red,        Opaque))),
+                                                    Rear  => (Colors => (others => (Blue,       Opaque))),
+                                                    Upper => (Colors => (others => (dark_Green, Opaque))),
+                                                    Lower => (Colors => (others => (Yellow,     Opaque))),
+                                                    Left  => (Colors => (others => (Cyan,       Opaque))),
+                                                    Right => (Colors => (others => (Magenta,    Opaque)))));
    the_Box : constant gel.Sprite.view
      := gel.Sprite.forge.new_Sprite ("demo.Box",
                                      gel.Sprite.World_view (the_Applet.gui_World),
-                                     math.Origin_3d,
+                                     math.Origin_3D,
                                      the_box_graphics_Model,
                                      the_box_physics_Model);
 begin
@@ -71,10 +71,10 @@ begin
    the_Applet.enable_simple_Dolly (1);                    -- Enable user camera control via keyboards.
    the_Applet.enable_Mouse (detect_Motion => False);      -- Enable the mouse.
 
-   the_Applet.gui_World.add (the_Ground,  and_children => False);               -- Add ground.
-   the_Ground.Site_is  ((0.0,  0.0,  0.0));
+   the_Applet.gui_World.add (the_Ground, and_children => False);               -- Add ground.
+   the_Ground.Site_is (math.Origin_3D);
 
-   the_Applet.gui_World.add (the_Box, and_children => False);                   -- Add ball.
+   the_Applet.gui_World.add (the_Box, and_Children => False);                   -- Add ball.
    the_Box.Site_is ((0.0, 10.0,  0.0));
 
    while the_Applet.is_open
@@ -87,7 +87,7 @@ begin
 exception
    when E : others =>
       new_Line;
-      put_Line ("Unhandled exception in main thread !");
-      put_Line (Ada.Exceptions.Exception_Information (E));
+      put_Line ("Unhandled exception in main thread ...");
+      put_Line (ada.Exceptions.exception_Information (E));
       new_Line;
 end launch_drop_Box_on_Box;
