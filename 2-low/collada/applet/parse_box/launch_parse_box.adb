1c688f5ece0474361cd14d7952df76516f89a418
Rod Kay <rodakay5@gmail.com>
2021-10-05 04:40:36 +1100

    collada.demo.parse_box: Add unreferenced aspect to 'the_Asset'.

--- 2-low/collada/applet/parse_box/launch_parse_box.adb
+++ 2-low/collada/applet/parse_box/launch_parse_box.adb
@@ -6,7 +6,8 @@ procedure launch_parse_Box
 -- Loads an xml file, parses it into a collada document.
 --
 is
-   the_Asset : collada.Document.item := collada.document.to_Document ("./box.dae");
+   the_Asset : collada.Document.item := collada.document.to_Document ("./box.dae")
+     with unreferenced;
 begin
    null;
 end launch_parse_Box;
